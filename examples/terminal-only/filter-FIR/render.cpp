4a8a53f94e00dce2a2b916a175478cfde010f828
Giulio Moro <giuliomoro@yahoo.it>
2016-06-27 14:24:14 +0100

    filter-IIR and filter-FIR are killable. Moved all the examlpes requiring keyboard input to terminal-only/ (which should be ignored by the IDE)

--- examples/terminal-only/filter-FIR/render.cpp
+++ examples/terminal-only/filter-FIR/render.cpp
@@ -119,8 +119,9 @@ bool initialise_trigger()
 	if((gTriggerSamplesTask = Bela_createAuxiliaryTask(&trigger_samples, 50, "bela-trigger-samples")) == 0)
 		return false;
 
-	rt_printf("Press 'a' to trigger sample, 's' to stop\n");
-	rt_printf("Press 'q' to quit\n");
+	rt_printf("Press 'a' <enter> to trigger sample,\n"
+			  "      's' <enter> to stop\n");
+	rt_printf("      'q' <enter> or ctrl-C to quit\n");
 
 	return true;
 }
@@ -132,28 +133,47 @@ bool initialise_trigger()
 
 void trigger_samples()
 {
-	// This is not a real-time task!
-	// Cos getchar is a system call, not handled by Xenomai.
-	// This task will be automatically down graded.
+	// This is not a real-time task because
+	// select() and scanf() are system calls, not handled by Xenomai.
+	// This task will be automatically down graded to "secondary mode"
+	// the first time it is executed.
 
 	char keyStroke = '.';
 
-	keyStroke =	getchar();
-	while(getchar()!='\n'); // to read the first stroke
+	fd_set readfds;
+    struct timeval tv;
+    int    fd_stdin;
+	fd_stdin = fileno(stdin);
+	while (!gShouldStop){
+		FD_ZERO(&readfds);
+		FD_SET(fileno(stdin), &readfds);
+		tv.tv_sec = 0;
+		tv.tv_usec = 1000;
+		fflush(stdout);
+		// Check if there are any characters ready to be read
+		int num_readable = select(fd_stdin + 1, &readfds, NULL, NULL, &tv);
+		// if there are, then read them
+		if(num_readable > 0){
+			scanf("%c", &keyStroke);
+			if(keyStroke != '\n'){ // filter out the "\n" (newline) character
+				switch (keyStroke)
+				{
+					case 'a':
+						gReadPtr = 0;
+						break;
+					case 's':
+						gReadPtr = -1;
+						break;
+					case 'q':
+						gShouldStop = true;
+						break;
+					default:
+						break;
+				}
 
-	switch (keyStroke)
-	{
-		case 'a':
-			gReadPtr = 0;
-			break;
-		case 's':
-			gReadPtr = -1;
-			break;
-		case 'q':
-			gShouldStop = true;
-			break;
-		default:
-			break;
+			}
+		}
+		usleep(1000);
 	}
 }
 
