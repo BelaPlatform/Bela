6ef25f70f09053de685416af4dfaae67f8247e24
Giulio Moro <giuliomoro@yahoo.it>
2016-08-15 21:31:48 +0100

    Properly killing IDE from gulp

--- ide/dev/gulpfile.js
+++ ide/dev/gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('rebuild-nodemodules', ['upload-nodemodules'], (callback) => {
 });
 
 gulp.task('killnode', (callback) => {
-	exec('ssh '+user+'@'+host+' "make -C Bela idestop"', (err) => {
+	exec('ssh '+user+'@'+host+' "make -C Bela idestop; killall node"', (err) => {
 		if (err) console.log('unable to stop node');
 		callback(); // finished task
 	});
