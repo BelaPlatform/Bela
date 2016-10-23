d0b0876a72659e46e426e54a07b770d565f96ced
Giulio Moro <giuliomoro@yahoo.it>
2016-10-24 00:54:36 +0100

    capeButtonClick kills current running process but not the screen it runs in, so that it can be used to restart it

--- resources/sh/Bela_capeButtonClick.sh
+++ resources/sh/Bela_capeButtonClick.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #this file is executed when the button on the Bela cape is pressed for less than 2 seconds.
-
-make -C /root/Bela stop
+BELA_AUDIO_THREAD_NAME=bela-audio
+PID=`grep $BELA_AUDIO_THREAD_NAME /proc/xenomai/stat | cut -d " " -f 5 | sed s/\s//g`; if [ -z $PID ]; then echo "No process to kill"; else echo "Killing old Bela process $PID"; kill -2 $PID; sleep 0.2; kill -9 $PID 2> /dev/null; fi; 
+killall scsynth 2>/dev/null& killall sclang 2>/dev/null&
