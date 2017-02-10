e2d75b8408eee65da9ef7b2775dd59660f43eb10
Giulio Moro <giuliomoro@yahoo.it>
2017-02-10 02:00:36 +0000

    Added --open and --get options to build_project.sh : open the file/folder with the system viewer and send a GET request to the IDE to build the current project

--- scripts/build_project.sh
+++ scripts/build_project.sh
@@ -37,6 +37,8 @@ WATCH=0
 HOST_SOURCE_PATH=
 FORCE=0
 EXPERT=0
+GET=0
+OPEN=0
 while [ -n "$1" ]
 do
 	case $1 in
@@ -73,6 +75,12 @@ do
 		--watch)
 			WATCH=1
 		;;
+		--get)
+			GET=1
+		;;
+		--open)
+			OPEN=1
+		;;
 		--help|-h|-\?)
 			usage
 			exit 0
@@ -96,17 +104,41 @@ done
 [ $FORCE -eq 1 ] && EXPERT=1
 
 # Check that we have a directory containing at least one source file
-# as an argument
-
 if [ -z "$HOST_SOURCE_PATH" ]
 then
 	usage
 	exit 2
 fi
 
-FIND_STRING="find $HOST_SOURCE_PATH -maxdepth 1 -type f "
+if [ "$OPEN" -eq 1 ]
+then
+	# try to open a file with the system viewer
+	OPEN_FILENAME="$HOST_SOURCE_PATH"
+	if [ -d "$OPEN_FILENAME" ]
+	then
+		# if it is a directory, let's try to guess a filename
+		for FILENAME in _main.pd render.cpp _main.scd main.py
+		do
+			TEST_OPEN_FILENAME="$HOST_SOURCE_PATH"/$FILENAME
+			[ -f "$TEST_OPEN_FILENAME" ] && OPEN_FILENAME=$TEST_OPEN_FILENAME
+		done
+	fi
+	case "$OSTYPE" in
+		darwin*)
+			echo Opening $OPEN_FILENAME
+			open "$OPEN_FILENAME"
+		;;
+	#TODO: add support for different OSes here.
+		*)
+			echo "\`--open\' option is not (yet) supported on your operating system"
+	esac
+fi
+# If a filename was given, strip the filename out
+[ -f "$HOST_SOURCE_PATH" ] && HOST_SOURCE_PATH=`dirname "$HOST_SOURCE_PATH"`
+
+FIND_STRING="find \"$HOST_SOURCE_PATH\" -maxdepth 1 -type f "
 EXTENSIONS_TO_FIND='\.cpp\|\.c\|\.S\|\.pd\|\.scd'
-FOUND_FILES=$($FIND_STRING 2>/dev/null | grep "$EXTENSIONS_TO_FIND")
+FOUND_FILES=$(eval $FIND_STRING 2>/dev/null | grep "$EXTENSIONS_TO_FIND")
 if [ -z "$FOUND_FILES" ]
 then
 	 printf "ERROR: Please provide a directory containing .c, .cpp, .S, .pd or .scd files.\n\n"
@@ -158,7 +190,7 @@ uploadBuildRun(){
 		# --no-t makes sure file timestamps are not preserved, so that the Makefile will not think that targets are up to date when replacing files on the BBB
 		#  with older files from the host. This will solve 99% of the issues with Makefile thinking a target is up to date when it is not.
 		echo "using rsync..."
-		rsync -ac --out-format="   %n" --no-t --delete-after --exclude=$BBB_PROJECT_NAME --exclude=build $HOST_SOURCE_PATH"/" "$BBB_NETWORK_TARGET_FOLDER/" #trailing slashes used here make sure rsync does not create another folder inside the target folder
+		rsync -ac --out-format="   %n" --no-t --delete-after --exclude="$BBB_PROJECT_NAME" --exclude=build "$HOST_SOURCE_PATH""/" "$BBB_NETWORK_TARGET_FOLDER/" #trailing slashes used here make sure rsync does not create another folder inside the target folder
 	fi
 
 	if [ $? -ne 0 ]
@@ -166,15 +198,21 @@ uploadBuildRun(){
 		printf "\nError while copying files\n"
 		exit
 	fi
-	# Make new Bela executable and run
-	MAKE_COMMAND="make --no-print-directory QUIET=true -C $BBB_BELA_HOME PROJECT='$BBB_PROJECT_NAME' CL='$COMMAND_ARGS' $BBB_MAKEFILE_OPTIONS"
-	if [ $RUN_PROJECT -eq 0 ]
+	if [ "$GET" -eq 1 ]
 	then
-		echo "Building project..."
-		ssh $BBB_ADDRESS "$MAKE_COMMAND"
+		echo "Asking the IDE to rebuild currently active project"
+		curl $BBB_HOSTNAME/build-project
 	else
-        echo "Building and running project..."
-	    case_run_mode
+		# Make new Bela executable and run
+		MAKE_COMMAND="make --no-print-directory QUIET=true -C $BBB_BELA_HOME PROJECT='$BBB_PROJECT_NAME' CL='$COMMAND_ARGS' $BBB_MAKEFILE_OPTIONS"
+		if [ $RUN_PROJECT -eq 0 ]
+		then
+			echo "Building project..."
+			ssh $BBB_ADDRESS "$MAKE_COMMAND"
+		else
+			echo "Building and running project..."
+			case_run_mode
+		fi
 	fi
 }
 # run it once and then (in case) start waiting for changes
@@ -186,7 +224,7 @@ if [ $WATCH -ne 0 ]; then
 		echo "Waiting for changes in $HOST_SOURCE_PATH, or press ctrl-c to terminate"
 		CORE_DIR="$SCRIPTDIR/../core"
 		INCLUDE_DIR="$SCRIPTDIR/../include"
-		wait_for_change $HOST_SOURCE_PATH "$reference_time_file" && {
+		wait_for_change "$HOST_SOURCE_PATH" "$reference_time_file" && {
 			echo "Content of "$HOST_SOURCE_PATH" has changed"
 		}
 		echo "Files changed"
