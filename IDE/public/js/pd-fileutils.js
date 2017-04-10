5f77b99e32e62fa90557c6ca2508c91ff18aec97
Giulio Moro <giuliomoro@yahoo.it>
2017-04-10 21:42:52 +0000

    Fixed pd-fileutils when [declare] is present. Closees #273.

--- IDE/public/js/pd-fileutils.js
+++ IDE/public/js/pd-fileutils.js
@@ -11207,8 +11207,12 @@ var recursParse = function(txt) {
     } else if (chunkType === '#X') {
       var elementType = tokens[1]
 
+      // ---- declare: ignore the declaration (if a obj declare is present, it will be handled as a regular object below) ---- //
+	  if (elementType === 'declare') {
+      } 
+
       // ---- restore : ends a canvas definition ---- //
-      if (elementType === 'restore') {
+      else if (elementType === 'restore') {
         var layout = {x: parseInt(tokens[2], 10), y: parseInt(tokens[3], 10)}
           , canvasType = tokens[4]
           , args = []
