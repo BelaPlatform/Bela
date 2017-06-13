062572168f9fc8c0928178130be9d218bbff386f
Giulio Moro <giuliomor@yahoo.it>
2017-06-13 02:02:29 +0000

    Actually closed #286: using free-running process, fixed interacting with IDE, removed useless IDE/bin/... files, linked the executable to resources/mode_switches_detector/..

--- resources/mode_switches_detector/mode_switches_detector.c
+++ resources/mode_switches_detector/mode_switches_detector.c
@@ -2,6 +2,7 @@
 #include <rtdk.h>
 #include <native/types.h>
 #include <native/task.h>
+#include <unistd.h>
 
 #ifdef _XENOMAI_TRANK_RTDK_H
 #define XENOMAI_MAJOR 3
@@ -12,25 +13,41 @@
 int main(){
 	const char* task_name = "bela-audio";
 	RT_TASK audio;
-	int ret = rt_task_bind(&audio, task_name, 0);
-	if(ret > 0){
-		fprintf(stderr, "An error occurred: %d. Task '%s' not running?", task_name);
-		return ret;
-	}
 	RT_TASK_INFO info;
-	rt_task_inquire(&audio, &info);
-	int prio;
-	int msw;
+	int count = 0;
+	int ret = 1;
+	while (ret != 0 && count < 30){
+		usleep(1000000);
+		ret = rt_task_bind(&audio, task_name, 0);
+		count += 1;
+	}
+	
+	usleep(500000);
+	if (ret != 0)
+		return 1;
+
+	int oldMsw = 0;
+	while(1){
+
+		ret = rt_task_inquire(&audio, &info);
+		if(ret != 0){
+			fprintf(stderr, "%s\n", strerror(-ret));
+			break;
+		}
+			
 #if XENOMAI_MAJOR == 2
-	prio = info.bprio;
-	msw = info.modeswitches;
+		int msw = info.modeswitches;
 #elif XENOMAI_MAJOR == 3
-	prio = info.prio;
-	msw = info.stat.msw;
+		int msw = info.stat.msw;
 #else 
 #error Xenomai version not supported
 #endif
-	printf("prio: %d\n", prio);
-	printf("modeswitches: %d\n", msw);
+		if (msw != oldMsw)
+		{
+			printf("%d\n", msw);
+			oldMsw = msw;
+		}
+		usleep(1000000);
+	}
 	return 0;
 }
