ab22beba36d568271756f3bc356efd626cf13ffb
transmogrifox <transmogrifox@cackleberrypines.net>
2017-07-12 14:24:46 -0700

    Update I2c_Codec.cpp
    Configure CODEC High-Pass filter for removal of DC offset on audio inputs.

--- core/I2c_Codec.cpp
+++ core/I2c_Codec.cpp
@@ -40,6 +40,12 @@ int I2c_Codec::initCodec()
 // it runs at 44.1kHz
 int I2c_Codec::startAudio(int dual_rate)
 {
+	// As a best-practice it's safer not to assume the implementer has issued initCodec()
+	// or has not otherwise modified codec registers since that call.
+	// Explicit Switch to config register page 0:
+	if(writeRegister(0x00, 0x00))	// Page Select Register
+		return 1;
+	
 	// see datasehet for TLV320AIC3104 from page 44
 	if(writeRegister(0x02, 0x00))	// Codec sample rate register: fs_ref / 1
 		return 1;
@@ -69,8 +75,6 @@ int I2c_Codec::startAudio(int dual_rate)
 		return 1;
 	if(writeRegister(0x0A, 0x00))	// Audio serial control register C: 0 bit offset
 		return 1;
-	if(writeRegister(0x0C, 0x00))	// Digital filter register: disabled
-		return 1;
 	if(writeRegister(0x0D, 0x00))	// Headset / button press register A: disabled
 		return 1;
 	if(writeRegister(0x0E, 0x00))	// Headset / button press register B: disabled
@@ -91,7 +95,11 @@ int I2c_Codec::startAudio(int dual_rate)
 
 	if(writeRegister(0x66, 0x02))	// Clock generation control register: use MCLK, PLL N = 2
 		return 1;
-
+	
+	//Set-up hardware high-pass filter for DC removal
+	if(configureDCRemovalIIR())
+		return 1;
+	
 	// wait for the codec to stabilize before unmuting the HP amp.
 	// this gets rid of the loud pop.
 	usleep(10000);
@@ -116,6 +124,88 @@ int I2c_Codec::startAudio(int dual_rate)
 		return 1;
 
 	running = true;
+	return 0;
+}
+
+//
+// Enable DC offset removal in CODEC hardware (High-Pass filter)
+// Datasheet reference: TLV320AIC3104, SLAS510F - FEB 2007, Rev DEC 2016
+// CODEC 1-pole Filter.  Datasheet calls it HPF but it can be any 1-pole filter
+// realizable by the following the transfer function:
+//           n0 + n1 * z^-1
+// H(z) =  ------------------    (10.3.3.2.1 - pg 26)
+//           d0 - d1 * z^-1
+// d0 =  32768 //defined by hardware (presumably fixed-point unity)
+// For the high-pass filter case (bilinear transform method),
+// d1 = n0 = -n1
+// d1 = d0*e^(-2*pi*fc/fs) = 32768*e^(-2*pi*8/44100) = 32730.7, round up
+// d1 =  32731 = 0x7FDB
+// n0 =  32731 = 0x7FDB
+// n1 =  -32731 = 0x8025 //2s complement
+//
+//    NOTE: Increasing the sampling rate will increase the filter cut-off proportionately.
+//          The selected cut-off should be acceptable up to 96 kHz sampling rate.
+//
+
+int I2c_Codec::configureDCRemovalIIR(){
+
+	//Explicit Switch to config register page 0:
+	if(writeRegister(0x00, 0x00))	//Page 1/Register 0: Page Select Register
+		return 1;
+
+	//
+	//  Config Page 0 commands
+	//
+	if(writeRegister(0x0C, 0x50))	// Digital filter register: enable HPF on L&R Channels
+		return 1;
+	if(writeRegister(0x6B, 0xC0))	// HPF coeff select register: Use programmable coeffs
+		return 1;
+
+	//Switch to config register page 1:
+	if(writeRegister(0x00, 0x01))	//Page 1/Register 0: Page Select Register
+		return 1;
+	//
+	//  Config Page 0 commands
+	//
+
+	//Left Channel HPF Coeffiecient Registers
+	//N0:
+	if(writeRegister(0x41, 0x7F))	// Page 1/Register 65: Left-Channel ADC High-Pass Filter N0 Coefficient MSB Register
+		return 1;
+	if(writeRegister(0x42, 0xDB))	//Page 1/Register 66: Left-Channel ADC High-Pass Filter N0 Coefficient LSB Register
+		return 1;
+	//N1:
+	if(writeRegister(0x43, 0x80))	//Page 1/Register 67: Left-Channel ADC High-Pass Filter N1 Coefficient MSB Register
+		return 1;
+	if(writeRegister(0x44, 0x25))	//Page 1/Register 68: Left-Channel ADC High-Pass Filter N1 Coefficient LSB Register
+		return 1;
+	//D1:
+	if(writeRegister(0x45, 0x7F))	//Page 1/Register 69: Left-Channel ADC High-Pass Filter D1 Coefficient MSB Register
+		return 1;
+	if(writeRegister(0x46, 0xDB))	//Page 1/Register 70: Left-Channel ADC High-Pass Filter D1 Coefficient LSB Register
+		return 1;
+
+	//Right Channel HPF Coeffiecient Registers
+	//N0:
+	if(writeRegister(0x47, 0x7F))	//Page 1/Register 71: Right-Channel ADC High-Pass Filter N0 Coefficient MSB Register
+		return 1;
+	if(writeRegister(0x48, 0xDB))	//Page 1/Register 72: Right-Channel ADC High-Pass Filter N0 Coefficient LSB Register
+		return 1;
+	//N1:
+	if(writeRegister(0x49, 0x80))	//Page 1/Register 73: Right-Channel ADC High-Pass Filter N1 Coefficient MSB Register
+		return 1;
+	if(writeRegister(0x4A, 0x25))	//Page 1/Register 74: Right-Channel ADC High-Pass Filter N1 Coefficient LSB Register
+		return 1;
+	//D1:
+	if(writeRegister(0x4B, 0x7F))	//Page 1/Register 75: Right-Channel ADC High-Pass Filter D1 Coefficient MSB Register
+		return 1;
+	if(writeRegister(0x4C, 0xDB))	//Page 1/Register 76: Right-Channel ADC High-Pass Filter D1 Coefficient LSB Register
+		return 1;
+
+	//Explicitly restore to config Page 0
+	if(writeRegister(0x00, 0x00))	//Page 1/Register 0: Page Select Register
+		return 1;
+
 	return 0;
 }
 
