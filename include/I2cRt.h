8dc25cacef17cf11bf3944d54e0ff7fc57a9a46a
Giulio Moro <giuliomoro@yahoo.it>
2017-12-16 04:00:18 +0000

    use I2cRt::setAddressRt() to set the address that will be used by the real-time-safe I2c transactions.

--- include/I2cRt.h
+++ include/I2cRt.h
@@ -28,6 +28,7 @@ public:
 	typedef struct _Msg{
 		MsgType type;
 		MsgStatus status;
+		int address;
 		int readSize;
 		int writeSize;
 		i2c_char_t* payload;
@@ -35,12 +36,15 @@ public:
 	int readRt(unsigned int readSize);
 	int writeRt(i2c_char_t* outbuf, unsigned int writeSize);
 	int writeReadRt(i2c_char_t* outbuf, unsigned int writeSize, unsigned int readSize);
+	void setAddressRt(int address);
 	int writeRegistersRt(i2c_char_t reg, i2c_char_t *values, unsigned int size);
 	int readRegistersRt(i2c_char_t reg, unsigned int size);
 	Msg retrieveMessage();
+	bool isIoBufferEmpty();
 private:
 	int msgToIo(MsgType type, i2c_char_t* outbuf, unsigned int writeSize, unsigned int readSize);
 	Msg failedReadingFromIo();
+	void drainToIo();
 	ring_buffer* toIo;
 	ring_buffer* fromIo;
 	volatile int* extIoShouldStop;
@@ -48,4 +52,5 @@ private:
 	bool ioShouldStop();
 	std::vector<i2c_char_t> inbuf;
 	pthread_t thread;
+	int i2cAddressRt;
 };
