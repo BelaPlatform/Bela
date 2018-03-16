51285d3224987d42e97e642e7fa24795b9f2f2c4
Giulio Moro <giuliomoro@yahoo.it>
2018-03-16 02:37:15 +0000

    First step towards adding support for detecting board (though currently the board is still #define'd in multiple places)
    (cherry picked from commit f382e060b971a86960a9166693d48a147041adf8)

---
+++ include/AudioCodec.h
@@ -0,0 +1,17 @@
+#ifndef AUDIOCODEC_H_
+#define AUDIOCODEC_H_
+
+class AudioCodec
+{
+public:
+	virtual ~AudioCodec() {};
+	virtual int initCodec() = 0;
+	virtual int startAudio(int parameter) = 0;
+	virtual int stopAudio() = 0;
+	virtual int setPga(float newGain, unsigned short int channel) = 0;
+	virtual int setDACVolume(int halfDbSteps) = 0;
+	virtual int setADCVolume(int halfDbSteps) = 0;
+	virtual int setHPVolume(int halfDbSteps) = 0;
+	virtual int disable() = 0;
+};
+#endif /* AUDIOCODEC_H_ */
