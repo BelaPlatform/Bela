4723a1dc4b1b53283e2be23425c3ba6d70030130
LBDonovan <liam.donovan@hotmail.co.uk>
2018-04-09 23:11:43 +0100

    WSServer: add streaming ability

--- core/WSServer.cpp
+++ core/WSServer.cpp
@@ -28,37 +28,52 @@ struct WSServerDataHandler : seasocks::WebSocket::Handler {
 	}
 };
 
+struct WSServerStreamHandler : seasocks::WebSocket::Handler {
+	WSServer* instance;
+	void onConnect(seasocks::WebSocket *socket) override 
+		{ instance->stream_connections.insert(socket); }
+	// void onData(WebSocket *, const char *data) override 
+		// { for (auto c : connections) c->send(data); }
+	void onDisconnect(seasocks::WebSocket *socket) override 
+		{ instance->stream_connections.erase(socket); }
+};
+
 void WSServer::ws_server_task_func(void* ptr){
 	printf("ws_server_task_func\n");
 	WSServer* instance = (WSServer*)ptr;
-	
-	auto logger = std::make_shared<seasocks::IgnoringLogger>();
-	instance->server = std::make_shared<seasocks::Server>(logger);
-	
-	instance->handler = std::make_shared<WSServerDataHandler>();
-	instance->handler->instance = instance;
-	instance->server->addWebSocketHandler(instance->address.c_str(), instance->handler);
-	
 	instance->server->serve("/dev/null", instance->port);
 	printf("server terminated\n");
 }
 
 void WSServer::ws_client_task_func(void* ptr, void* buf, int size){
-	// printf("ws_client_task_func\n");
 	WSServer* instance = (WSServer*)ptr;
 	instance->server->execute([instance, buf, size]{
-		// printf("HELLO\n");
 		for (auto c : instance->connections){
 			c->send((uint8_t*) buf, size);
 		}
 	});
-	// printf("ws_client_task_func done\n");
+}
+
+void WSServer::ws_stream_task_func(void* ptr, void* buf, int size){
+	WSServer* instance = (WSServer*)ptr;
+	instance->server->execute([instance, buf, size]{
+		for (auto c : instance->stream_connections){
+			c->send((uint8_t*) buf, size);
+		}
+	});
 }
 
 void WSServer::setup(int _port, std::string _address, void(*_callback)(void* buf, int size)){
 	port = _port;
 	address = _address;
 	callback = _callback;
+	
+	auto logger = std::make_shared<seasocks::IgnoringLogger>();
+	server = std::make_shared<seasocks::Server>(logger);
+	
+	handler = std::make_shared<WSServerDataHandler>();
+	handler->instance = this;
+	server->addWebSocketHandler(_address.c_str(), handler);
 	
 	ws_server_task = std::unique_ptr<AuxTaskNonRT>(new AuxTaskNonRT());
 	ws_server_task->create(std::string("WSServer_")+std::to_string(_port), WSServer::ws_server_task_func, this);
@@ -68,11 +83,32 @@ void WSServer::setup(int _port, std::string _address, void(*_callback)(void* buf
 	ws_client_task->create(std::string("WSClient_")+std::to_string(_port), WSServer::ws_client_task_func, this);
 }
 
+void WSServer::streamTo(std::string _address, int stream_buffer_size){
+	streamAddress = _address;
+
+	streamBuffer.reserve(stream_buffer_size);
+	
+	stream_handler = std::make_shared<WSServerStreamHandler>();
+	stream_handler->instance = this;
+	server->addWebSocketHandler(_address.c_str(), stream_handler);
+	
+	ws_stream_task = std::unique_ptr<AuxTaskNonRT>(new AuxTaskNonRT());
+	ws_stream_task->create(std::string("WSStream_")+std::to_string(port), WSServer::ws_stream_task_func, this);
+}
+
 void WSServer::send(std::string str){
 	ws_client_task->schedule(str.c_str());
 }
 void WSServer::send(void* buf, int num_bytes){
 	ws_client_task->schedule(buf, num_bytes);
+}
+
+void WSServer::stream(float data){
+	streamBuffer.push_back(data);
+	if (streamBuffer.size() >= streamBuffer.capacity()){
+		ws_stream_task->schedule((void*)&streamBuffer[0], streamBuffer.size()*sizeof(float));
+		streamBuffer.clear();
+	}
 }
 
 void WSServer::cleanup(){
--- include/WSServer.h
+++ include/WSServer.h
@@ -2,6 +2,9 @@
 #include <string>
 #include <memory>
 #include <set>
+#include <vector>
+
+#define WSSERVER_STREAM_BUFFERSIZE 1024
 
 // forward declarations for faster render.cpp compiles
 namespace seasocks{
@@ -10,17 +13,23 @@ namespace seasocks{
 }
 class AuxTaskNonRT;
 struct WSServerDataHandler;
+struct WSServerStreamHandler;
 
 class WSServer{
 	friend struct WSServerDataHandler;
+	friend struct WSServerStreamHandler;
 	public:
 		WSServer();
 		~WSServer();
 		
 		void setup(int port, std::string address, void(*on_receive)(void* buf, int size));
 		
+		void streamTo(std::string address, int size = WSSERVER_STREAM_BUFFERSIZE);
+		
 		void send(std::string str);
 		void send(void* buf, int num_bytes);
+		
+		void stream(float data);
 		
 	private:
 		void cleanup();
@@ -31,10 +40,18 @@ class WSServer{
 		std::shared_ptr<WSServerDataHandler> handler;
 		std::set<seasocks::WebSocket*> connections;
 		
+		std::string streamAddress;
+		std::vector<float> streamBuffer;
+		std::shared_ptr<WSServerStreamHandler> stream_handler;
+		std::set<seasocks::WebSocket*> stream_connections;
+		
 		std::unique_ptr<AuxTaskNonRT> ws_server_task;
 		static void ws_server_task_func(void* ptr);
 		void(*callback)(void* buf, int size);
 		
 		std::unique_ptr<AuxTaskNonRT> ws_client_task;
 		static void ws_client_task_func(void* ptr, void* buf, int size);
+		
+		std::unique_ptr<AuxTaskNonRT> ws_stream_task;
+		static void ws_stream_task_func(void* ptr, void* buf, int size);
 };
