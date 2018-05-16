5153552ad672a8f5c27e502b81d82a7dc2c53924
Giulio Moro <giuliomoro@yahoo.it>
2018-05-16 11:32:16 +0100

    Support for CSound. Requires to manually install Csound for Bela from https://github.com/csound/csound/releases

--- IDE/public/js/bundle.js
+++ IDE/public/js/bundle.js
@@ -2888,6 +2888,9 @@ var ProjectView = function (_View) {
 			form.push('<input id="popup-SC" type="radio" name="project-type" data-type="SC">');
 			form.push('<label for="popup-SC">SuperCollider</label>');
 			form.push('</br>');
+			form.push('<input id="popup-CS" type="radio" name="project-type" data-type="CS">');
+			form.push('<label for="popup-CS">Csound</label>');
+			form.push('</br>');
 			form.push('<input type="text" placeholder="Enter your project name">');
 			form.push('</br>');
 			form.push('<button type="submit" class="button popup-save">Create</button>');
