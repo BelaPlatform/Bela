1d8d5c23169e1943c4f55d4420de6439d933fe90
LBDonovan <liam.donovan@hotmail.co.uk>
2018-05-19 21:41:40 +0100

    IDE: ProcessManager: tests for process start, finish and stdio events

--- IDE/test/ProcessManager.spec.ts
+++ IDE/test/ProcessManager.spec.ts
@@ -3,6 +3,8 @@ import * as mock from 'mock-fs';
 import * as process_manager from '../src/ProcessManager';
 import * as file_manager from '../src/FileManager';
 import * as processes from '../src/IDEProcesses';
+import * as socket_manager from '../src/SocketManager';
+import * as child_process from 'child_process';
 var sinon = require('sinon');
 
 should();
@@ -181,6 +183,183 @@ describe('ProcessManager', function(){
 				sinon.restore();
 			});
 		});
+		describe('#stop', function(){
+			let exec_stub: any;
+			beforeEach(function(){
+				exec_stub = sinon.stub(child_process, 'exec');
+			});
+			it('should stop all running processes using their stop() methods', function(){
+				sinon.stub(processes.run, 'get_status').returns(true);
+				sinon.stub(processes.build, 'get_status').returns(true);
+				sinon.stub(processes.syntax, 'get_status').returns(true);
+				let run_stop_stub = sinon.stub(processes.run, 'stop');
+				let build_stop_stub = sinon.stub(processes.build, 'stop');
+				let syntax_stop_stub = sinon.stub(processes.syntax, 'stop');
+				process_manager.stop();
+				run_stop_stub.callCount.should.equal(1);
+				build_stop_stub.callCount.should.equal(1);
+				syntax_stop_stub.callCount.should.equal(1);
+				exec_stub.callCount.should.equal(0);
+			});
+			it('should call make stop if no processes are running', function(){
+				let run_stop_stub = sinon.stub(processes.run, 'stop');
+				let build_stop_stub = sinon.stub(processes.build, 'stop');
+				let syntax_stop_stub = sinon.stub(processes.syntax, 'stop');
+				process_manager.stop();
+				run_stop_stub.callCount.should.equal(0);
+				build_stop_stub.callCount.should.equal(0);
+				syntax_stop_stub.callCount.should.equal(0);
+				exec_stub.callCount.should.equal(1);
+				exec_stub.getCall(0).args.should.deep.equal(['make -C /root/Bela/ stop']);
+			});
+			afterEach(function(){
+				sinon.restore();
+			});
+		});
+		describe('#syntax process', function(){
+			let broadcast_stub: any;
+			beforeEach(function(){
+				broadcast_stub = sinon.stub(socket_manager, 'broadcast');
+			});
+			it('should broadcast a status event and object when it starts', function(){
+				processes.syntax.emit('start');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						checkingSyntax	: false,
+						building	: false,
+						buildProject	: '',
+						running		: false,
+						runProject	: ''
+					}
+				]);
+			});
+			it('should broadcast a status event and object including syntaxError when it finished', function(){
+				processes.syntax.emit('finish', 'test_stderr');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						checkingSyntax	: false,
+						building	: false,
+						buildProject	: '',
+						running		: false,
+						runProject	: '',
+						syntaxError	: 'test_stderr'
+					}
+				]);
+			});
+			afterEach(function(){
+				sinon.restore();
+			});
+		});
+		describe('#build process', function(){
+			let broadcast_stub: any;
+			beforeEach(function(){
+				broadcast_stub = sinon.stub(socket_manager, 'broadcast');
+			});
+			it('should broadcast a status event and object when it starts', function(){
+				processes.build.emit('start');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						checkingSyntax	: false,
+						building	: false,
+						buildProject	: '',
+						running		: false,
+						runProject	: ''
+					}
+				]);
+			});
+			it('should broadcast a status event and object including syntaxError when it finished', function(){
+				processes.build.emit('finish', 'test_stderr');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						checkingSyntax	: false,
+						building	: false,
+						buildProject	: '',
+						running		: false,
+						runProject	: '',
+						syntaxError	: 'test_stderr'
+					}
+				]);
+			});
+			it('should broadcast a build log of its stdout', function(){
+				processes.build.emit('stdout', 'test_stdout');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						buildLog: 'test_stdout'
+					}
+				]);
+			});
+			afterEach(function(){
+				sinon.restore();
+			});
+		});
+		describe('#run process', function(){
+			let broadcast_stub: any;
+			beforeEach(function(){
+				broadcast_stub = sinon.stub(socket_manager, 'broadcast');
+			});
+			it('should broadcast a status event and object when it starts', function(){
+				processes.run.emit('start');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						checkingSyntax	: false,
+						building	: false,
+						buildProject	: '',
+						running		: false,
+						runProject	: ''
+					}
+				]);
+			});
+			it('should broadcast a status event and object when it finished', function(){
+				processes.run.emit('finish');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						checkingSyntax	: false,
+						building	: false,
+						buildProject	: '',
+						running		: false,
+						runProject	: ''
+					}
+				]);
+			});
+			it('should broadcast a log of its stdout', function(){
+				processes.run.emit('stdout', 'test_stdout');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						belaLog: 'test_stdout'
+					}
+				]);
+			});
+			it('should broadcast a log of its stderr', function(){
+				processes.run.emit('stderr', 'test_stderr');
+				broadcast_stub.callCount.should.equal(1);
+				broadcast_stub.getCall(0).args.should.deep.equal([
+					'status',
+					{
+						belaLogErr: 'test_stderr'
+					}
+				]);
+			});
+			afterEach(function(){
+				sinon.restore();
+			});
+		});
+
 	});
 });
 
