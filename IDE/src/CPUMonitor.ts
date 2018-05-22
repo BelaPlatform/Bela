ea35312fa16138d8848a828825e759ff35acdeb3
LBDonovan <liam.donovan@hotmail.co.uk>
2018-05-22 22:09:29 +0100

    IDE: CPUMonitor: use setTimeout instead of setInterval to prevent cpu_monitor overloading the processor

--- IDE/src/CPUMonitor.ts
+++ IDE/src/CPUMonitor.ts
@@ -7,37 +7,43 @@ import * as ide_settings from './IDESettings';
 // every second with the cpu usage as a parameter
 
 let name: string;
-let interval: NodeJS.Timer;
+let timeout: NodeJS.Timer;
 let found_pid: boolean = false;
 let root_pid: number;
 let main_pid: number;
 let callback: (cpu: any) => void;
+let stopped: boolean = false;
 
 export function start(pid: number, project: string, cb: (cpu: any)=>void){
 	root_pid = pid;
 	// the process name gets cut off at 15 chars
 	name = project.substring(0, 15);
 	callback = cb;
-	interval = setInterval( () => interval_func(), 1000);
+	stopped = false;
+	timeout = setTimeout( () => timeout_func(), 1000);
 }
 
 export function stop(){
-	clearInterval(interval);
+	if (timeout) clearTimeout(timeout);
+	stopped = true;
 }
 
 // this function keeps trying every second to find the correct pid
 // once it has, it uses ps to get the cpu usage, and calls the callback
-async function interval_func(){
+async function timeout_func(){
 	if (!(await ide_settings.get_setting('cpuMonitoring')))
 		return;
 	let cpu: any = '0';
 	if (!found_pid){
 		// use pidtree to find all the child pids of the make process
+		console.log('pidtree');
 		await pidtree(root_pid, {root: true})
 			.then(async (pids: any) => {
+				console.log(pids);
 				// look through the pids to see if any of them belong to a process with the right name
 				for (let pid of pids){
 					let test_name = (await name_from_pid(pid) as string).trim();
+					console.log(pid, test_name, name);
 					if (test_name === name){
 						main_pid = pid;
 						found_pid = true;
@@ -55,7 +61,10 @@ async function interval_func(){
 				found_pid = false;
 			});
 	}
+	if(stopped)
+		return;
 	callback(cpu);
+	setTimeout(timeout_func);
 }
 
 // returns the name of the process corresponding to the pid passed in to it
