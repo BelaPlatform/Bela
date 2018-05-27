b57325335944c299737bd23a46d400d1cf854b18
LBDonovan <liam.donovan@hotmail.co.uk>
2018-05-27 17:08:20 +0100

    IDE: fix xenomai version test to ignore minor version

--- IDE/test/IDE.spec.ts
+++ IDE/test/IDE.spec.ts
@@ -12,7 +12,7 @@ describe('Top-level IDE functions', function(){
 	describe('#get_xenomai_version', function(){
 		it('should return the correct version string', async function(){
 			let version: string = await IDE.get_xenomai_version();
-			version.should.satisfy((ver: string) => (ver.includes('3.0.5') || ver.includes('2.6')));
+			version.should.satisfy((ver: string) => (ver.includes('3.0') || ver.includes('2.6')));
 		});
 	});
 	describe('#shutdown', function(){
