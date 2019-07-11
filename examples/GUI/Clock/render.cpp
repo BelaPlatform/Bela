7e0fd99ab7d915a6afcd9278053a34bbd0158859
adanlbenito <adanl.benito@gmail.com>
2019-07-11 00:07:40 +0000

    Added comments and explanation to GUI examples

--- examples/GUI/Clock/render.cpp
+++ examples/GUI/Clock/render.cpp
@@ -1,19 +1,21 @@
-
 /*
-GUI clock
-=========
-New GUI fuctionality for Bela!
-Is this project there is a sketch.js file which is a p5.js file that is rendered
-in a browser tab. Click the GUI button in the IDE to see the rendering of this file.
-
-This example sends a buffer of data from the c++ file to the p5.js file:
-`gui.sendBuffer(0, dateTimeComponents);`
-
-The p5.js file displays the received data (time).
-
-If you want to edit sketch.js you can do so in the browser but must write your
-p5.js code in instance mode.
-*/
+ * 
+ * GUI clock
+ * =========
+ *
+ * New GUI fuctionality for Bela!
+ *
+ * Is this project you can find a sketch.js file which is a p5.js file that is rendered
+ * in a browser tab. Click the GUI button (next to the Scope button) in the IDE to see the rendering of this file.
+ * 
+ * This example sends a buffer of data from the Bela render to the browser via a web socket:
+ * 	`gui.sendBuffer(0, dateTimeComponents);`
+ * 
+ * The p5.js file displays the received data (time and date).
+ * 
+ * If you want to edit sketch.js you can do so in the browser but must write your p5.js code in instance mode.
+ * 
+ */
 
 #include <Bela.h>
 #include <typeinfo>
@@ -23,12 +25,16 @@ p5.js code in instance mode.
 #include <iterator>
 #include <sstream>
 #include <math.h>
-
 #include <time.h>
 
+// GUI object declaration
 Gui gui;
 
+// Vector that holds the different components for the date
+// Order after filling the values using calcDate() should be: [year, month, day, hours, minutes, seconds, milliseconds]
 std::vector<int> dateTimeComponents;
+
+// Time period (in seconds) after which date will be updated and sent 
 float gTimePeriod = 0.01;
 
 // Get current date/time, format is YYYY-MM-DD.HH:mm:ss
@@ -45,9 +51,12 @@ const std::vector<std::string> currentDateTime() {
     strftime(time, sizeof(time), "%T", &tstruct);
 	dateTime.emplace_back(date);
 	dateTime.emplace_back(time);
+
+    // Returns vector holding 2 strings: [ "Y-m-d", "h-M-s"] 
     return dateTime;
 }
 
+// Utility function to split strings by a delimiter character
 std::vector<std::string> split(const std::string& s, char delimiter)
 {
    std::vector<std::string> tokens;
@@ -60,23 +69,7 @@ std::vector<std::string> split(const std::string& s, char delimiter)
    return tokens;
 }
 
-bool setup(BelaContext *context, void *userData)
-{
-	std::vector<std::string> dateComponents = split(currentDateTime()[0], '-');
-	for(unsigned int i=0; i<dateComponents.size(); i++)
-		dateTimeComponents.push_back(atoi(dateComponents[i].c_str()));
-
-	std::vector<std::string> timeComponents = split(currentDateTime()[1], ':');
-	for(unsigned int i=0; i<timeComponents.size(); i++)
-		dateTimeComponents.push_back(atoi(timeComponents[i].c_str()));
-
-	dateTimeComponents.push_back(0);// millisedcons
-
-	gui.setup(5555, "gui", context->projectName);
-
-	return true;
-}
-
+// Update date [Y, m, d, h, M, s, ms] based on the number of samples elapsed
 void calcDate(unsigned int samplesElapsed, unsigned int sampleRate, std::vector<int> &date)
 {
 	unsigned int miliseconds = floor(1000 * samplesElapsed / sampleRate) + date[6];
@@ -94,6 +87,25 @@ void calcDate(unsigned int samplesElapsed, unsigned int sampleRate, std::vector<
 	date[0] = floor(months/12) + date[0];
 }
 
+bool setup(BelaContext *context, void *userData)
+{
+	// Get Y-m-d from current time, split and place in date components vector
+	std::vector<std::string> dateComponents = split(currentDateTime()[0], '-');
+	for(unsigned int i=0; i<dateComponents.size(); i++)
+		dateTimeComponents.push_back(atoi(dateComponents[i].c_str()));
+	// Get h:M:s from current time, split and place in date components vector
+	std::vector<std::string> timeComponents = split(currentDateTime()[1], ':');
+	for(unsigned int i=0; i<timeComponents.size(); i++)
+		dateTimeComponents.push_back(atoi(timeComponents[i].c_str()));
+	// Add milliseconds to date components
+	dateTimeComponents.push_back(0);// milliseconds
+
+	// Setup GUI. By default, the Bela GUI runs on port 5555 and address 'gui'
+	gui.setup(5555, "gui", context->projectName);
+
+	return true;
+}
+
 void render(BelaContext *context, void *userData)
 {
 	static unsigned int count = 0;
@@ -101,7 +113,9 @@ void render(BelaContext *context, void *userData)
 	for(unsigned int n = 0; n < context->audioFrames; n++) {
 		if (count >= gTimePeriod*context->audioSampleRate)
 		{
+			// Calculate date based on elapsed number of seconds and update date and time components
 			calcDate(count, context->audioSampleRate, dateTimeComponents);
+			// Send int buffer holding the date and time components to the GUI with index 0
 			gui.sendBuffer(0, dateTimeComponents);
 			count = 0;
 		}
