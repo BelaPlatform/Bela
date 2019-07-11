1346cadac91c55637af81166a70d2b44a894e35b
adanlbenito <adanl.benito@gmail.com>
2019-07-11 00:30:04 +0000

    Added documentation to Gui class in Gui library

--- libraries/Gui/Gui.h
+++ libraries/Gui/Gui.h
@@ -43,30 +43,75 @@ class Gui
 		~Gui();
 
 		int setup(unsigned int port, std::string address);
+		/**
+		 * Sets the web socket communication between server and client.
+		 * Two different web sockets communications will be configured, one for control data and the other one for raw binary data. 
+		 * @param port: Port on which to stablish the the web socket communication.
+		 * @param address: Base address used to stalish the web socket communication.
+		 * @param projectName: Project name to be sent to via the web-socket to the client.
+		 * @returns: O if web sockets have been configured.
+		 **/
 		int setup(unsigned int port, std::string address, std::string projectName);
 		void cleanup();
 
 		bool isConnected(){ return wsIsConnected; };
 
 		// BUFFERS
-		// Returns buffer ID
-		// Such ID is generated automatically based on the number of buffers
+		/**
+		 * Sets the buffer type and size of the container.
+		 * @param bufferType: type of the buffer, can be float (f), int (i) or char (c)
+		 * @param size: Maximum number of elements that the buffer can hold.
+		 * @retur: buffer ID, generated automatically based on the number of buffers and
+		 * 	the order on which they have been created.
+		 **/
 		unsigned int setBuffer(char bufferType, unsigned int size);
+		/**
+		 * Returns a DataBuffer object which represents a typed buffer holding a number
+		 * of bytes that an be retrieved in different formats.
+		 * @param bufferId: buffer ID
+		 **/
 		DataBuffer& getDataBuffer(unsigned int bufferId);
 
+		/**
+		 * Set callback to parse control data received from the client.
+		 * @param callback: Callback to be called whenever new control data is received.
+		 * 	Takes a byte buffer, the size of the buffer and a pointer as parameters.
+		 * 	The first two parameters are used for the data received on the web-socket.
+		 *	The third parameter is used to access data structures pointed by the user.
+		 *
+		 * @param customControlData: pointer to data structure to be pased to the callback.
+		 **/
 		void setControlDataCallback(std::function<bool(const char*, int, void*)> callback, void* customControlData=nullptr){
 			customOnControlData = callback;
 			userControlData = customControlData;
 		};
+		/**
+		 * Set callback to parse binary data received from the client.
+		 * @param callback: Callback to be called whenever new data is received.
+		 * 	Takes a byte buffer, the size of the buffer and a pointer as parameters.
+		 * 	The first two parameters are used for the data received on the web-socket.
+		 *	The third parameter is used to access data structures pointed by the user.
+		 *
+		 * @param customBinaryData: pointer to data structure to be pased to the callback.
+		 **/
+
 		void setBinaryDataCallback(std::function<bool(const char*, int, void*)> callback, void* customBinaryData=nullptr){
 			customOnData = callback;
 			userBinaryData = customBinaryData;
 		};
 
-
+		/**
+		 * Sends a buffer (a vector) through the web-socket to the client with a given ID.
+		 * @param bufferId: given buffer ID
+		 * @param buffer: vector of arbitrary length and type
+		 **/
 		template<typename T, typename A>
 		void sendBuffer(unsigned int bufferId, std::vector<T,A> & buffer);
-
+		/**
+		 * Sends a buffer (an array) through the web-socket to the client with a given ID.
+		 * @param bufferId: given buffer ID
+		 * @param buffer: array of arbitrary size and type
+		 **/
 		template <typename T, size_t N>
 		void sendBuffer(unsigned int bufferId, T (&buffer)[N]);
 
