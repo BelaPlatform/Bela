BelaPlatform/Bela

commit 59204ce33986fa262646a4d17843ee1b53f2e14a

FEAT: Order Examples - Now if you want you can order the Examples across main folders from the root of the examples folder as well as the individual example folders themselves. The Basics folder has a demo in it. This may render the numerical naming convention obsolete so I've done a demo version which renames all of the folders in Basics. There MUST be a order.json file in the root of all example folders or they won't attach the contents to the list. An empty array is fine as they're treated as orphan folders and get attached to the array in standard alphabetical format.

dev/src/Views/ProjectView.js
@@ -2,7 +2,7 @@ var View = require('./View');
 var popup = require('../popup');
 var sanitise = require('../utils').sanitise;
 var json = require('../site-text.json');
-var example_order = require('../example_order.json');
+var example_order = require('../../../examples/order.json');
 
 class ProjectView extends View {
 
@@ -165,20 +165,28 @@ class ProjectView extends View {
 		var $examples = $('[data-examples]');
     var oldListOrder = examplesDir;
     var newListOrder = [];
+    var orphans = [];
 
 		$examples.empty();
 
 		if (!examplesDir.length) return;
 
-    oldListOrder.forEach(item => {
-      example_order.forEach(new_item => {
+    example_order.forEach(new_item => {
+      oldListOrder.forEach(item => {
         if (new_item == item.name) {
           newListOrder.push(item);
-          oldListOrder.splice(oldListOrder.indexOf(item), 1);
+          item.moved = true;
         }
       });
     });
-    var orderedList = newListOrder.concat(oldListOrder);
+
+    oldListOrder.forEach(item => {
+      if (item.moved != true) {
+        orphans.push(item);
+      }
+    });
+
+    var orderedList = newListOrder.concat(orphans);
 
 		for (let item of orderedList){
       let parentButton = $('<button></button>').addClass('accordion').attr('data-accordion-for', item.name).html(item.name + ':');
@@ -187,34 +195,91 @@ class ProjectView extends View {
       let childUl = $('<ul></ul>').addClass('example-list');
       let childDiv = $('<div></div>').addClass('panel').attr('data-accordion', item.name);
 
-			for (let child of item.children){
-				if (child && child.length && child[0] === '.') continue;
-        let childLi = $('<li></li>');
-				childLi.html(child).attr('data-example-link', item.name + '/' + child)
-					.on('click', (e) => {
+      var childOrder = [];
+      for (let child of item.children){
+        childOrder.push({"name": child});
+      }
+      var newChildOrder = [];
+      var oldChildOrder = [];
+      var correctedChildOrder = [];
+      var childOrphans = [];
 
-						if (this.exampleChanged){
-							this.exampleChanged = false;
-							popup.exampleChanged( () => {
-								this.emit('message', 'project-event', {
-									func: 'openExample',
-									currentProject: item.name + '/' + child
-								});
-								$('.selectedExample').removeClass('selectedExample');
-								$(e.target).addClass('selectedExample');
-							}, undefined, 0, () => this.exampleChanged = true );
-							return;
-						}
+      var that = this;
+      for (let child of childOrder){
+        if (child.name == "order.json") {
+          $.ajax({
+            type: "GET",
+            url: "/examples/" + item.name + "/" + child.name,
+            dataType: "json",
+            success: function(text){
+              newChildOrder = [];
+              text.forEach(item => {
+                newChildOrder.push({"name": item});
+              });
 
-						this.emit('message', 'project-event', {
-							func: 'openExample',
-							currentProject: item.name + '/' + child
-						});
-						$('.selectedExample').removeClass('selectedExample');
-						$(e.target).addClass('selectedExample');
-					});
-          childLi.appendTo(childUl);
-			}
+              item.children.forEach(item => {
+                if (item !== "order.json") {
+                  oldChildOrder.push({"name": item});
+                }
+              });
+
+              newChildOrder.forEach(new_item => {
+                oldChildOrder.forEach(old_item => {
+                  if (new_item.name == old_item.name) {
+                    correctedChildOrder.push(new_item);
+                    old_item.moved = true;
+                  }
+                });
+              });
+
+              oldChildOrder.forEach(old_item => {
+                if (old_item.moved != true) {
+                  childOrphans.push(old_item);
+                }
+              });
+
+              childOrder = correctedChildOrder.concat(childOrphans);
+
+              for (var i = 0; i < childOrder.length; i++) {
+                child = childOrder[i].name;
+                var link = item.name + '/' + child;
+                let childLi = $('<li></li>');
+                childLi.html(child).attr('data-example-link', link)
+                .on('click', (e) => {
+                  link = e.target.dataset.exampleLink;
+                  if (this.exampleChanged){
+                    this.exampleChanged = false;
+                    popup.exampleChanged( (link) => {
+                      that.emit('message', 'project-event', {
+                        func: 'openExample',
+                        currentProject: link
+                      });
+                      $('.selectedExample').removeClass('selectedExample');
+                      $(e.target).addClass('selectedExample');
+                    }, undefined, 0, () => this.exampleChanged = true );
+                    return;
+                  }
+
+                  that.emit('message', 'project-event', {
+                    func: 'openExample',
+                    currentProject: link
+                  });
+                  $('.selectedExample').removeClass('selectedExample');
+                  $(e.target).addClass('selectedExample');
+                });
+                childLi.appendTo(childUl);
+              }
+
+              childOrder = [];
+              newChildOrder = [];
+              oldChildOrder = [];
+              correctedChildOrder = [];
+              childOrphans = [];
+            }
+          });
+
+        }
+      }
       // per section
       // item.name -> parentDiv $examples
       parentButton.appendTo(parentLi);

commit 116f71955987102127be3008b02da9ac9b4b7bf3

Opening an example after editing an example correctly prompts the user for confirm.
Rectifies a bug introduced in 59204ce33986fa262646a4d17843ee1b53f2e14a.
Closes https://github.com/BelaPlatform/Bela-private/issues/202

IDE/public/js/bundle.js
@@ -3381,8 +3381,6 @@ var ProjectView = function (_View) {
                   url: "/examples/" + item.name + "/" + child.name,
                   dataType: "json",
                   success: function success(text) {
-                    var _this7 = this;
-
                     newChildOrder = [];
                     text.forEach(function (item) {
                       newChildOrder.push({ "name": item });
@@ -3417,8 +3415,8 @@ var ProjectView = function (_View) {
                       var childLi = $('<li></li>');
                       childLi.html(child).attr('data-example-link', link).on('click', function (e) {
                         link = e.target.dataset.exampleLink;
-                        if (_this7.exampleChanged) {
-                          _this7.exampleChanged = false;
+                        if (that.exampleChanged) {
+                          that.exampleChanged = false;
                           popup.exampleChanged(function (link) {
                             that.emit('message', 'project-event', {
                               func: 'openExample',
@@ -3426,8 +3424,8 @@ var ProjectView = function (_View) {
                             });
                             $('.selectedExample').removeClass('selectedExample');
                             $(e.target).addClass('selectedExample');
-                          }, undefined, 0, function () {
-                            return _this7.exampleChanged = true;
+                          }, link, 0, function () {
+                            return that.exampleChanged = true;
                           });
                           return;
                         }

dev/src/Views/ProjectView.js
@@ -250,8 +250,8 @@ _exampleList(examplesDir){
                 childLi.html(child).attr('data-example-link', link)
                 .on('click', (e) => {
                   link = e.target.dataset.exampleLink;
-                  if (this.exampleChanged){
-                    this.exampleChanged = false;
+                  if (that.exampleChanged){
+                    that.exampleChanged = false;
                     popup.exampleChanged( (link) => {
                       that.emit('message', 'project-event', {
                         func: 'openExample',
@@ -259,7 +259,7 @@ _exampleList(examplesDir){
                       });
                       $('.selectedExample').removeClass('selectedExample');
                       $(e.target).addClass('selectedExample');
-                    }, undefined, 0, () => this.exampleChanged = true );
+                    }, link, 0, () => that.exampleChanged = true );
                     return;
                   }
 