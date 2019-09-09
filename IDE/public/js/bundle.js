BelaPlatform/Bela

commit fdcdfbfb775aa55bea3117fbeee11ffdc2c4da44

Fix for the dropdown menu multi select problem

IDE/public/js/bundle.js
@@ -2921,8 +2921,11 @@ var ProjectView = function (_View) {
 				});
 				return;
 			}
+			console.log("sc reporting in");
+			console.log($element);
+			console.log('name: ' + $element.data('name'));
 
-			this.emit('message', 'project-event', { func: $element.data().func, currentProject: $element.val() });
+			this.emit('message', 'project-event', { func: $element.data().func, currentProject: $element.data('name') });
 		}
 	}, {
 		key: 'buttonClicked',
@@ -3058,7 +3061,7 @@ var ProjectView = function (_View) {
 			$projects.attr('size', projLen - 1);
 			for (var i = 0; i < projLen; i++) {
 				if (projects[i] && projects[i] !== 'undefined' && projects[i] !== 'exampleTempProject' && projects[i][0] !== '.') {
-					$('<option></option>').addClass('projectManager').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).appendTo($projects).on('click', function () {
+					$('<li></li>').addClass('projectManager proj-li').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).attr('data-name', projects[i]).appendTo($projects).on('click', function () {
 						$(this).blur();
 						$(this).parent().parent().removeClass('show');
 					});
@@ -4456,8 +4459,7 @@ var View = function (_EventEmitter) {
 				});
 			}
 		}
-
-		_this.$elements.filter('select').on('change', function (e) {
+		_this.$elements.on('click', 'li.proj-li', function (e) {
 			return _this.selectChanged($(e.currentTarget), e);
 		});
 		_this.$elements.filter('input').on('input', function (e) {
@@ -4533,6 +4535,9 @@ var View = function (_EventEmitter) {
 				}
 			}
 		}
+	}, {
+		key: 'testSelect',
+		value: function testSelect() {}
 	}, {
 		key: 'selectChanged',
 		value: function selectChanged(element, e) {}
@@ -5427,7 +5432,6 @@ function example(cb, arg, delay, cancelCb) {
 	popup.title('Save your changes?');
 	popup.subtitle('Warning: Any unsaved changes will be lost');
 	popup.body('You have made changes to an example project. If you continue, your changes will be lost. To keep your changes, click cancel and then Save As in the project manager tab');
-	popup.code('<h1>Hello World!</h1>');
 	var form = [];
 	form.push('<button type="submit" class="button popup confirm">Continue</button>');
 	form.push('<button type="button" class="button popup cancel">Cancel</button>');
@@ -5435,6 +5439,7 @@ function example(cb, arg, delay, cancelCb) {
 	popup.form.append(form.join('')).off('submit').on('submit', function (e) {
 		e.preventDefault();
 		setTimeout(function () {
+			console.log(arg);
 			cb(arg);
 		}, delay);
 		popup.hide();

dev/src/Views/ProjectView.js
@@ -23,8 +23,11 @@ class ProjectView extends View {
 			});
 			return;
 		}
+    console.log("sc reporting in");
+    console.log($element);
+    console.log('name: ' + $element.data('name'));
 
-		this.emit('message', 'project-event', {func: $element.data().func, currentProject: $element.val()})
+		this.emit('message', 'project-event', {func: $element.data().func, currentProject: $element.data('name')})
 
 	}
 
@@ -149,7 +152,7 @@ class ProjectView extends View {
     $projects.attr('size', (projLen - 1));
 		for (let i=0; i < projLen; i++){
 			if (projects[i] && projects[i] !== 'undefined' && projects[i] !== 'exampleTempProject' && projects[i][0] !== '.'){
-				$('<option></option>').addClass('projectManager').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).appendTo($projects).on('click', function() {
+				$('<li></li>').addClass('projectManager proj-li').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).attr('data-name', projects[i]).appendTo($projects).on('click', function() {
           $(this).blur();
           $(this).parent().parent().removeClass('show');
         });

IDE/public/index.html
@@ -107,8 +107,10 @@
               <span class="dropdown" data-projects>
 							  <button class="dropbtn" data-dropdown-for="project">Your projects</button>
                 <div class="dropdown-content" data-dropdown="project">
-                  <select class="projectManager" id="projects" data-projects-select data-func="openProject" multiple>
-  						    </select>
+                  <ul class="projectManager" id="projects" data-projects-select data-func="openProject">
+                  </ul>
+                  <!-- <select class="projectManager" id="projects" data-projects-select data-func="openProject" multiple>
+  						    </select> -->
                 </div>
 							</span>
               <button id="new" class="create-project projectManager" data-func="newProject">New Project</button>

dev/src/Views/View.js
@@ -20,8 +20,7 @@ class View extends EventEmitter{
 				});
 			}
 		}
-
-		this.$elements.filter('select').on('change', (e) => this.selectChanged($(e.currentTarget), e));
+		this.$elements.on('click', 'li.proj-li', (e) => this.selectChanged($(e.currentTarget), e));
 		this.$elements.filter('input').on('input', (e) => this.inputChanged($(e.currentTarget), e));
 		this.$elements.filter('input[type=checkbox]').on('change', (e) => this.inputChanged($(e.currentTarget), e));
 		this.$elements.filter('button').on('click', (e) => this.buttonClicked($(e.currentTarget), e));
@@ -42,7 +41,7 @@ class View extends EventEmitter{
 			}
 		}
 	}
-
+	testSelect(){}
 	selectChanged(element, e){}
 	buttonClicked(element, e){}
 

dev/src/popup.js
@@ -53,7 +53,6 @@ function example(cb, arg, delay, cancelCb){
 	popup.title('Save your changes?');
 	popup.subtitle('Warning: Any unsaved changes will be lost');
   popup.body('You have made changes to an example project. If you continue, your changes will be lost. To keep your changes, click cancel and then Save As in the project manager tab');
-  popup.code('<h1>Hello World!</h1>');
 	var form = [];
 	form.push('<button type="submit" class="button popup confirm">Continue</button>');
 	form.push('<button type="button" class="button popup cancel">Cancel</button>');
@@ -61,6 +60,7 @@ function example(cb, arg, delay, cancelCb){
 	popup.form.append(form.join('')).off('submit').on('submit', e => {
 		e.preventDefault();
 		setTimeout(function(){
+			console.log(arg);
 			cb(arg);
 		}, delay);
 		popup.hide();

commit 425abee0e7180597f4470c99b7ec62f2fd47efac

Opening a project after editing an example correctly prompts the user to confirm, and accepts the user's action.
Rectifies a bug introduced in fdcdfbfb775aa55bea3117fbeee11ffdc2c4da44.
Closes https://github.com/BelaPlatform/Bela-private/issues/193.

IDE/public/js/bundle.js
@@ -3135,7 +3135,7 @@ var ProjectView = function (_View) {
       if (this.exampleChanged) {
         this.exampleChanged = false;
         popup.exampleChanged(function () {
-          _this2.emit('message', 'project-event', { func: $element.data().func, currentProject: $element.val() });
+          _this2.emit('message', 'project-event', { func: $element.data().func, currentProject: $element.data().name });
         }, undefined, 0, function () {
           _this2.exampleChanged = true;
         });
@@ -3282,7 +3282,7 @@ var ProjectView = function (_View) {
       $projects.attr('size', projLen - 1);
       for (var i = 0; i < projLen; i++) {
         if (projects[i] && projects[i] !== 'undefined' && projects[i] !== 'exampleTempProject' && projects[i][0] !== '.') {
-          $('<li></li>').addClass('projectManager proj-li').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).attr('data-name', projects[i]).appendTo($projects).on('click', function () {
+          $('<li></li>').addClass('projectManager proj-li').attr('data-func', 'openProject').html(projects[i]).attr('data-name', projects[i]).appendTo($projects).on('click', function () {
             $(this).blur();
             $(this).parent().parent().removeClass('show');
           });

dev/src/Views/ProjectView.js
@@ -17,7 +17,7 @@ class ProjectView extends View {
       if (this.exampleChanged){
         this.exampleChanged = false;
         popup.exampleChanged( () => {
-        this.emit('message', 'project-event', {func: $element.data().func, currentProject: $element.val()});
+        this.emit('message', 'project-event', {func: $element.data().func, currentProject: $element.data().name});
       }, undefined, 0, () => {
         this.exampleChanged = true;
       });
@@ -153,7 +153,7 @@ class ProjectView extends View {
     $projects.attr('size', (projLen - 1));
     for (let i=0; i < projLen; i++){
       if (projects[i] && projects[i] !== 'undefined' && projects[i] !== 'exampleTempProject' && projects[i][0] !== '.'){
-        $('<li></li>').addClass('projectManager proj-li').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).attr('data-name', projects[i]).appendTo($projects).on('click', function() {
+        $('<li></li>').addClass('projectManager proj-li').attr('data-func', 'openProject').html(projects[i]).attr('data-name', projects[i]).appendTo($projects).on('click', function() {
           $(this).blur();
           $(this).parent().parent().removeClass('show');
         });