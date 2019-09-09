BelaPlatform/Bela

commit 413e1fb82cad571a6cb2b9cf93129a2952e877b3

FEAT: IDE Unfurl - Slowly replacing more of the functionality IDs with data bindings

IDE/public/js/bundle.js
@@ -628,7 +628,7 @@ socket.on('init', function (data) {
 	models.project.setData({ projectList: data.projects, exampleList: data.examples, currentProject: data.settings.project });
 	models.settings.setData(data.settings);
 
-	$('#runOnBoot').val(data.boot_project);
+	$('data-run-on-boot').val(data.boot_project);
 
 	models.settings.setKey('xenomaiVersion', data.xenomai_version);
 
@@ -837,12 +837,6 @@ models.project.on('change', function (data, changedKeys) {
 		// status changes reflected here
 		models.status.on('change', function (data, changedKeys) {
 			if (changedKeys.indexOf('running') !== -1 || changedKeys.indexOf('building') !== -1) {
-				// if (data.running)
-				// 	$('[data-current-status]').html('Running: '+data.runProject);
-				// else if (data.building)
-				// 	$('[data-current-status]').html('Building: '+data.buildProject);
-				// else
-				// 	$('[data-current-status]').html('');
 				if (data.running) {
 					$('[data-current-status-title]').html('Running: ');
 					$('[data-current-status]').html(data.runProject);
@@ -859,13 +853,22 @@ models.project.on('change', function (data, changedKeys) {
 		// set the browser tab title
 		$('title').html((data.fileName ? data.fileName + ', ' : '') + projectName);
 		// set the top-line stuff
-		$('#top-open-project').html(projectName ? 'Project: ' + projectName : '');
-		$('#top-open-file').html(data.fileName ? 'File: ' + data.fileName : '');
+		$('[data-current-project]').html(projectName ? projectName : '');
+		$('[data-current-file]').html(data.fileName ? data.fileName : '');
 
 		// status changes reflected here
 		models.status.on('change', function (data, changedKeys) {
 			if (changedKeys.indexOf('running') !== -1 || changedKeys.indexOf('building') !== -1) {
-				if (data.running) $('#top-bela-status').html('Running: ' + data.runProject);else if (data.building) $('#top-bela-status').html('Building: ' + data.buildProject);else $('#top-bela-status').html('');
+				if (data.running) {
+					$('[data-current-status-title]').html('Running: ');
+					$('[data-current-status]').html(data.runProject);
+				} else if (data.building) {
+					$('[data-current-status-title]').html('Building: ');
+					$('[data-current-status]').html(data.buildProject);
+				} else {
+					$('[data-current-status]').html('');
+					$('[data-current-status-title]').html('');
+				}
 			}
 		});
 	}

dev/src/IDE-browser.js
@@ -211,7 +211,7 @@ socket.on('init', (data) => {
 	models.project.setData({projectList: data.projects, exampleList: data.examples, currentProject: data.settings.project});
 	models.settings.setData(data.settings);
 
-	$('#runOnBoot').val(data.boot_project);
+  $('data-run-on-boot').val(data.boot_project);
 
 	models.settings.setKey('xenomaiVersion', data.xenomai_version);
 
@@ -405,12 +405,6 @@ models.project.on('change', (data, changedKeys) => {
     // status changes reflected here
     models.status.on('change', (data, changedKeys) => {
     	if (changedKeys.indexOf('running') !== -1 || changedKeys.indexOf('building') !== -1){
-    		// if (data.running)
-    		// 	$('[data-current-status]').html('Running: '+data.runProject);
-    		// else if (data.building)
-    		// 	$('[data-current-status]').html('Building: '+data.buildProject);
-    		// else
-    		// 	$('[data-current-status]').html('');
         if (data.running) {
     			$('[data-current-status-title]').html('Running: ');
           $('[data-current-status]').html(data.runProject);
@@ -427,18 +421,22 @@ models.project.on('change', (data, changedKeys) => {
     // set the browser tab title
     $('title').html((data.fileName ? data.fileName+', ' : '') + projectName);
     // set the top-line stuff
-    $('#top-open-project').html(projectName ? 'Project: '+ projectName : '');
-    $('#top-open-file').html(data.fileName ? 'File: '+ data.fileName : '');
+    $('[data-current-project]').html(projectName ? projectName : '');
+    $('[data-current-file]').html(data.fileName ?  data.fileName : '');
 
     // status changes reflected here
     models.status.on('change', (data, changedKeys) => {
     	if (changedKeys.indexOf('running') !== -1 || changedKeys.indexOf('building') !== -1){
-    		if (data.running)
-    			$('#top-bela-status').html('Running: '+data.runProject);
-    		else if (data.building)
-    			$('#top-bela-status').html('Building: '+data.buildProject);
-    		else
-    			$('#top-bela-status').html('');
+        if (data.running) {
+    			$('[data-current-status-title]').html('Running: ');
+          $('[data-current-status]').html(data.runProject);
+    		} else if (data.building) {
+          $('[data-current-status-title]').html('Building: ');
+    			$('[data-current-status]').html(data.buildProject);
+    		} else {
+    			$('[data-current-status]').html('');
+          $('[data-current-status-title]').html('');
+        }
     	}
     });
   }

commit b5e0bf12635b0f10e2576886b1a03a9bff04a6bb

Correctly displaying the project that is currently set to run on boot.
Rectifies an issue introduced in 413e1fb82cad571a6cb2b9cf93129a2952e877b3.
Closes https://github.com/BelaPlatform/Bela-private/issues/172.

IDE/public/js/bundle.js
@@ -633,7 +633,7 @@ socket.on('init', function (data) {
 	models.project.setData({ projectList: data.projects, exampleList: data.examples, libraryList: data.libraries, currentProject: data.settings.project });
 	models.settings.setData(data.settings);
 
-	$('data-run-on-boot').val(data.boot_project);
+	$('[data-run-on-boot]').val(data.boot_project);
 
 	models.settings.setKey('xenomaiVersion', data.xenomai_version);
 

dev/src/IDE-browser.js
@@ -216,7 +216,7 @@ socket.on('init', (data) => {
 	models.project.setData({projectList: data.projects, exampleList: data.examples, libraryList: data.libraries,  currentProject: data.settings.project});
 	models.settings.setData(data.settings);
 
-  $('data-run-on-boot').val(data.boot_project);
+	$('[data-run-on-boot]').val(data.boot_project);
 
 	models.settings.setKey('xenomaiVersion', data.xenomai_version);
 