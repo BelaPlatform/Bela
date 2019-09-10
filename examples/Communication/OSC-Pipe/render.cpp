79daf3aa696437dfd125196b4f79470178c08ed9
Giulio Moro <giuliomoro@yahoo.it>
2019-09-10 01:57:04 +0000

    Added Pipe class. Added example of Pipe + OSC.
    Closes https://github.com/BelaPlatform/Bela-private/issues/191

---
+++ examples/Communication/OSC-Pipe/render.cpp
@@ -0,0 +1,126 @@
+/**
+\example OSC-pipe/render.cpp
+
+Open Sound Control
+------------------
+
+This example shows an implementation of OSC (Open Sound Control) which was
+developed at UC Berkeley Center for New Music and Audio Technology (CNMAT).
+It is very similar to Communication/OSC, but it uses a rt-safe pipe to
+exchange data between the thread that receives the incoming OSC messages
+and the audio thread.
+
+It is designed to run alongside resources/osc/osc.js.
+For the example to work, run in a terminal on the board
+```
+node /root/Bela/resources/osc/osc.js
+```
+
+The OSC receiver port on which to receive is set in `setup()`
+via `oscReceiver.setup()`. Likewise the OSC port on which to
+send is set in `oscSender.setup()`.
+
+In `setup()` an OSC message is sent to address `/osc-setup`, it then waits
+1 second for a reply on `/osc-setup-reply`.
+
+in `render()` the code receives OSC messages, parses them, and sends
+back an acknowledgment.
+*/
+
+#include <Bela.h>
+#include <libraries/OscSender/OscSender.h>
+#include <libraries/OscReceiver/OscReceiver.h>
+#include <libraries/Pipe/Pipe.h>
+
+Pipe oscPipe;
+
+OscReceiver oscReceiver;
+OscSender oscSender;
+int localPort = 7562;
+int remotePort = 7563;
+const char* remoteIp = "127.0.0.1";
+
+void on_receive(oscpkt::Message* msg)
+{
+	// we make a copy of the incoming message and we send it down the pipe to the real-time thread
+	oscpkt::Message* incomingMsg = new oscpkt::Message(msg);
+	oscPipe.writeNonRt(incomingMsg);
+
+	// the real-time thread sends back to us the pointer once it is done with it
+	oscpkt::Message* returnedMsg;
+	while(oscPipe.readNonRt(returnedMsg) > 0)
+	{
+		delete returnedMsg;
+	}
+}
+
+bool setup(BelaContext *context, void *userData)
+{
+	oscPipe.setup("incomingOsc");
+	oscReceiver.setup(localPort, on_receive);
+	oscSender.setup(remotePort, remoteIp);
+
+	// the following code sends an OSC message to address /osc-setup
+	oscSender.newMessage("/osc-setup").send();
+
+	printf("Waiting for handshake ....\n");
+	// we want to stop our program and wait for a new message to come in.
+	// therefore, we set the pipe to blocking mode.
+	oscPipe.setBlockingNonRt(false);
+	oscPipe.setBlockingRt(true);
+	oscPipe.setTimeoutMsRt(1000);
+	oscpkt::Message* msg = nullptr;
+	int ret = oscPipe.readRt(msg);
+	bool ok = false;
+	if(ret > 0) {
+		if(msg && msg->match("/osc-setup-reply"))
+		{
+			printf("handshake received!\n");
+			ok = true;
+		}
+		delete msg;
+	}
+	if(!ok) {
+		fprintf(stderr, "No handshake received: %d\n", ret);
+		return false;
+	}
+	// in the remainder of the program, we will be calling readRt() from render(), and we want it
+	// to return immediately if there are no new messages available. We therefore set the
+	// pipe to non-blocking mode
+	oscPipe.setBlockingRt(false);
+
+	return true;
+}
+
+void render(BelaContext *context, void *userData)
+{
+	oscpkt::Message* msg;
+	// read incoming messages from the pipe
+	while(oscPipe.readRt(msg) > 0)
+	{
+		if(msg && msg->match("/osc-test")){
+			int intArg;
+			float floatArg;
+			msg->arg().popInt32(intArg).popFloat(floatArg).isOkNoMoreArgs();
+			rt_printf("received a message with int %i and float %f\n", intArg, floatArg);
+			// the call below is not real-time safe, as it may allocate memory. We should not be calling it from here,
+			// If you see your mode switches (MSW) increase over time, you should really get it out of here.
+			oscSender.newMessage("/osc-acknowledge").add(intArg).add(4.2f).add(std::string("OSC message received")).send();
+		}
+		oscPipe.writeRt(msg); // return the pointer to the other thread, where it will be destroyed
+	}
+}
+
+void cleanup(BelaContext *context, void *userData)
+{
+	oscpkt::Message* returnedMsg;
+	// drain the pipes, so that any objects trapped in there can be appropriately destroyed
+	while(oscPipe.readRt(returnedMsg) > 0)
+	{
+		delete returnedMsg;
+	}
+	while(oscPipe.readNonRt(returnedMsg) > 0)
+	{
+		delete returnedMsg;
+	}
+}
---
+++ libraries/Pipe/Pipe.cpp
@@ -0,0 +1,207 @@
+#include "Pipe.h"
+#include <xenomai_wraps.h>
+#include <stdexcept>
+#include <sys/select.h>
+std::string Pipe::defaultName;
+
+Pipe::Pipe(const std::string& pipeName, size_t size, bool newBlockingRt, bool newBlockingNonRt)
+{
+	if(!setup(pipeName, size, newBlockingRt, newBlockingNonRt))
+		throw std::runtime_error((std::string("Pipe: ")+std::string(" failed to initialize. NOTE: cannot call setup() or the constructor before Xenomai has been initialised\n")).c_str());
+}
+
+bool Pipe::setup(const std::string& pipeName, size_t size, bool newBlockingRt, bool newBlockingNonRt)
+{
+	pipeSize = size;
+
+	name = "p_" + pipeName;
+	int ret = createXenomaiPipe(name.c_str(), pipeSize);
+	if(ret < 0)
+	{
+		fprintf(stderr, "Unable to create pipe %s with %u bytes: (%i) %s\n", name.c_str(), pipeSize, ret, strerror(ret));
+		return false;
+	}
+	pipeSocket = ret;
+	path = "/proc/xenomai/registry/rtipc/xddp/" + name;
+	// no idea why, but a usleep(0) is needed here. Give it a bit more time,
+	// just in case
+	usleep(10000);
+	unsigned int blockingFlag = blockingNonRt ? 0 : O_NONBLOCK;
+	fd = open(path.c_str(), O_RDWR | blockingFlag);
+	if(fd < 0)
+	{
+		fprintf(stderr, "Unable to open pipe %s: (%i) %s\n", path.c_str(), errno, strerror(errno));
+		//TODO: close the pipe
+		return false;
+	
+	}
+	setBlockingRt(newBlockingRt);
+	setBlockingNonRt(newBlockingNonRt);
+	return true;
+}
+
+void Pipe::setBlockingRt(bool blocking)
+{
+	blockingRt = blocking;
+	int flags = __wrap_fcntl(pipeSocket, F_GETFL);
+	if(blocking)
+	{
+		flags ^= O_NONBLOCK;
+	} else {
+		flags |= O_NONBLOCK;
+	}
+	if(int ret = __wrap_fcntl(pipeSocket, F_SETFL, flags))
+	{
+		fprintf(stderr, "Unable to set socket non blocking\n");
+	}
+}
+
+void Pipe::setBlockingNonRt(bool blocking)
+{
+	blockingNonRt = blocking;
+	int flags = fcntl(fd, F_GETFL);
+	if(blocking)
+	{
+		flags ^= O_NONBLOCK;
+	} else {
+		flags |= O_NONBLOCK;
+	}
+	if(int ret = fcntl(fd, F_SETFL, flags))
+	{
+		fprintf(stderr, "Unable to set socket non blocking\n");
+	}
+}
+
+void Pipe::setTimeoutMsRt(double timeoutMs)
+{
+	timeoutMsRt = timeoutMs;
+}
+
+void Pipe::setTimeoutMsNonRt(double timeoutMs)
+{
+	timeoutMsNonRt = timeoutMs;
+}
+
+void Pipe::cleanup()
+{
+	close(fd);
+	__wrap_close(pipeSocket);
+}
+
+bool Pipe::_writeNonRt(void* ptr, size_t size)
+{
+	ssize_t ret = write(fd, (void*)ptr, size);
+	if(ret < 0 || ret != size)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool Pipe::_writeRt(void* ptr, size_t size)
+{
+	ssize_t ret = __wrap_send(pipeSocket, (void*)ptr, size, 0);
+	if(ret < 0 || ret != size)
+	{
+		return false;
+	}
+	return true;
+}
+
+ssize_t Pipe::_readRt(void* ptr, size_t size)
+{
+	bool doIt = false;
+	int ret = 0;
+	if(blockingRt)
+	{
+		struct timeval tv;
+		tv.tv_sec = ((unsigned int)timeoutMsRt) / 1000;
+		tv.tv_usec = (timeoutMsRt - tv.tv_sec * 1000.f) * 1000.f;
+		fd_set fdSet;
+		FD_ZERO(&fdSet);
+		FD_SET(pipeSocket, &fdSet);
+		ret = __wrap_select(pipeSocket + 1, &fdSet, NULL, NULL, &tv);
+		if(1 == ret && FD_ISSET(pipeSocket, &fdSet))
+			doIt = true;
+	}
+	if(!blockingRt || doIt){
+		ret = __wrap_recv(pipeSocket, ptr, size, 0);
+	}
+	return ret;
+}
+
+ssize_t Pipe::_readNonRt(void* ptr, size_t size)
+{
+	return read(fd, ptr, size);
+}
+
+#if 0
+// tests
+#include <stdlib.h>
+#include <array>
+#undef NDEBUG
+#include <assert.h>
+
+template <typename T1, typename T2>
+static bool areEqual(const T1& vec1, const T2& vec2)
+{
+	if(vec1.size() != vec2.size())
+		return false;	
+	for(unsigned int n = 0; n < vec1.size(); ++n)
+	{
+		if(vec1[n] != vec2[n])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+template <typename T>
+static void scramble(T& vec)
+{
+	for(unsigned int n = 0; n < vec.size(); ++n)
+	{
+		vec[n] = rand();
+	}
+}
+
+int testPipe()
+{
+	printf("testPipe\n");
+	Pipe pipe("assaaa", 8192, true, false);
+	std::array<float, 1000> payload;
+	{
+		//Rt to NonRt
+		scramble(payload);
+		bool success = pipe.writeRt(payload);
+		assert(success);
+		std::array<float, payload.size()> rec;
+		int ret = pipe.readNonRt(rec.data(), rec.size());
+		assert(ret == rec.size());
+		assert(areEqual(payload, rec));
+		ret = pipe.readNonRt(rec.data(), rec.size());
+		assert(areEqual(payload, rec));
+		assert(ret != rec.size());
+		assert(ret < 0);
+	}
+	printf("second\n");
+	{
+		//NonRt to Rt
+		scramble(payload);
+		bool success = pipe.writeNonRt(payload);
+		assert(success);
+		std::array<float, payload.size()> rec;
+		int ret = pipe.readRt(rec.data(), rec.size());
+		assert(ret == rec.size());
+		assert(areEqual(payload, rec));
+		ret = pipe.readRt(rec.data(), rec.size());
+		assert(areEqual(payload, rec));
+		assert(0 == ret);
+	}
+
+	printf("Test for Pipe successful\n");
+	exit (0);
+}
+#endif
+
