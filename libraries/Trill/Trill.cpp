25b26bcb5f18d877175e675eabe65bed58145b13
adanlbenito <adanl.benito@gmail.com>
2020-01-23 17:47:24 +0000

    Added HEX device to  multitouch functionality

--- libraries/Trill/Trill.cpp
+++ libraries/Trill/Trill.cpp
@@ -273,7 +273,7 @@ int Trill::readLocations() {
 		prepareForDataRead();
 
 	uint8_t bytesToRead = kNormalLengthDefault;
-	if(device_type_ == SQUARE)
+	if(device_type_ == SQUARE || device_type_ == HEX)
 		bytesToRead = kNormalLength2D;
 	int bytesRead = ::read(i2C_file, dataBuffer, kNormalLengthDefault);
 	if (bytesRead != kNormalLengthDefault)
@@ -292,7 +292,7 @@ int Trill::readLocations() {
 	}
 	num_touches_ = locations;
 
-	if(device_type_ == SQUARE)
+	if(device_type_ == SQUARE || device_type_ == HEX)
 	{
 		// Look for the number of horizontal touches in 2D sliders
 		// which might be different from number of vertical touches
@@ -320,7 +320,7 @@ int Trill::numberOfTouches()
 // Number of horizontal touches for Trill 2D
 int Trill::numberOfHorizontalTouches()
 {
-	if(mode_ != CENTROID || device_type_ != SQUARE)
+	if(mode_ != CENTROID || device_type_ != SQUARE) || device_type_ != HEX
 		return 0;
 
 	// Upper 4 bits hold number of horizontal touches
@@ -362,7 +362,7 @@ int Trill::touchSize(uint8_t touch_num)
 
 int Trill::touchHorizontalLocation(uint8_t touch_num)
 {
-	if(mode_ != CENTROID || device_type_ != SQUARE)
+	if(mode_ != CENTROID || device_type_ != SQUARE || device_type_ != HEX)
 		return -1;
 	if(touch_num >= MAX_TOUCH_1D_OR_2D)
 		return -1;
@@ -375,7 +375,7 @@ int Trill::touchHorizontalLocation(uint8_t touch_num)
 
 int Trill::touchHorizontalSize(uint8_t touch_num)
 {
-	if(mode_ != CENTROID || device_type_ != SQUARE)
+	if(mode_ != CENTROID || device_type_ != SQUARE || device_type_ != HEX)
 		return -1;
 	if(touch_num >= MAX_TOUCH_1D_OR_2D)
 		return -1;
