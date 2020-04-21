20759929ed00c0dc20f33789bb3a17ceeab83c0b
adanlbenito <adanl.benito@gmail.com>
2020-04-21 17:06:01 +0000

    Fix indentation

--- IDE/public/gui/js/BelaControl.js
+++ IDE/public/gui/js/BelaControl.js
@@ -2,185 +2,185 @@ import BelaWebSocket from './BelaWebSocket.js'
 import GuiHandler from './GuiHandler.js'
 
 export default class BelaControl extends BelaWebSocket {
-    constructor(port=5555, address='gui_control', ip=location.host) {
-        super(port, address, ip)
+	constructor(port=5555, address='gui_control', ip=location.host) {
+		super(port, address, ip)
 
-        this.projectName = null;
-        this.sliders = [];
-        this.selectors = [];
-        this.gui = null;
-        this.gui_prototype = {}
-        this.target = new EventTarget();
-        this.events = [
-            new CustomEvent('new-slider', {
-                detail: {
-                    id: null
-                }
-            }),
-            new CustomEvent('new-select', {
-                detail: {
-                    id: null
-                }
-            }),
-            new CustomEvent('new-connection', {
-                detail: {
-                    projectName: null
-                }
-            }),
-            new CustomEvent('new-controller', {
-                detail: {
-                    name: null
-                }
-            }),
-            new CustomEvent('custom', {
-                detail: {
-                    data: null
-                }
-            }),
-            new CustomEvent('slider-changed', {
-                detail: { }
-            }),
-        ];
+		this.projectName = null;
+		this.sliders = [];
+		this.selectors = [];
+		this.gui = null;
+		this.gui_prototype = {}
+		this.target = new EventTarget();
+		this.events = [
+			new CustomEvent('new-slider', {
+				detail: {
+					id: null
+				}
+			}),
+			new CustomEvent('new-select', {
+				detail: {
+					id: null
+				}
+			}),
+			new CustomEvent('new-connection', {
+				detail: {
+					projectName: null
+				}
+			}),
+			new CustomEvent('new-controller', {
+				detail: {
+					name: null
+				}
+			}),
+			new CustomEvent('custom', {
+				detail: {
+					data: null
+				}
+			}),
+			new CustomEvent('slider-changed', {
+				detail: { }
+			}),
+		];
 
-        this.handler = new GuiHandler(this);
-    }
+		this.handler = new GuiHandler(this);
+	}
 
-    addGui(name) {
-        if(this.gui == null || typeof this.gui == 'undefined') {
-            this.gui = new this.handler.creator('gui-container', this.handler.iframeEl.contentDocument, this.handler.iframeEl.contentWindow);
-            this.gui.sliderCallback = this.sliderCallback;
-        }
-        let panel
-        if(this.gui.panels.length < 1) {
-            panel = this.gui.newPanel(name);
-        }
-    }
+	addGui(name) {
+		if(this.gui == null || typeof this.gui == 'undefined') {
+			this.gui = new this.handler.creator('gui-container', this.handler.iframeEl.contentDocument, this.handler.iframeEl.contentWindow);
+			this.gui.sliderCallback = this.sliderCallback;
+		}
+		let panel
+		if(this.gui.panels.length < 1) {
+			panel = this.gui.newPanel(name);
+		}
+	}
 
-    addSlider(sliderObject) {
-        if(this.gui != null) {
-            let param_exist = typeof(this.gui.parameters[0]) != 'undefined'
-            param_exist = param_exist && sliderObject.controller in this.gui.parameters[0]
+	addSlider(sliderObject) {
+		if(this.gui != null) {
+			let param_exist = typeof(this.gui.parameters[0]) != 'undefined'
+			param_exist = param_exist && sliderObject.controller in this.gui.parameters[0]
 
-            if(!param_exist || !(sliderObject.name in this.gui.parameters[0][sliderObject.controller])) {
-                    delete Object.assign(sliderObject, {['guiId']: sliderObject['controller'] })['controller'];
-                    this.gui.newSlider(sliderObject)
-            }
-        }
-    }
+			if(!param_exist || !(sliderObject.name in this.gui.parameters[0][sliderObject.controller])) {
+				delete Object.assign(sliderObject, {['guiId']: sliderObject['controller'] })['controller'];
+				this.gui.newSlider(sliderObject)
+			}
+		}
+	}
 
-    onData(data, parsedData) {
-        console.log("Data!", parsedData)
+	onData(data, parsedData) {
+		console.log("Data!", parsedData)
 
-        let that = this;
-        (async function() {
-            await new Promise(resolve => that.target.addEventListener('gui-ready', function(){
-                console.log("New event -> gui-ready")
-                resolve();
-            }));
-            that.target.removeEventListener('gui-ready', function(){
-                resolve();
-            });
-            console.log("____GUI PROTOTYPES____")
-            if(!(Object.keys(that.gui_prototype).length === 0 && that.gui_prototype.constructor === Object)) {
-                for (let p in that.gui_prototype) {
-                    that.addGui.bind(that)
-                    that.addGui(p)
-                    that.gui_prototype[p]['sliders'].forEach(s => {
-                        that.addSlider(s)
-                    });
-                    delete that.gui_prototype[p]
-                }
-            }
-        })();
+		let that = this;
+		(async function() {
+			await new Promise(resolve => that.target.addEventListener('gui-ready', function(){
+				console.log("New event -> gui-ready")
+				resolve();
+			}));
+			that.target.removeEventListener('gui-ready', function(){
+				resolve();
+			});
+			console.log("____GUI PROTOTYPES____")
+			if(!(Object.keys(that.gui_prototype).length === 0 && that.gui_prototype.constructor === Object)) {
+				for (let p in that.gui_prototype) {
+					that.addGui.bind(that)
+					that.addGui(p)
+					that.gui_prototype[p]['sliders'].forEach(s => {
+						that.addSlider(s)
+					});
+					delete that.gui_prototype[p]
+				}
+			}
+		})();
 
-            if (parsedData.event == 'connection') {
-                console.log('_____NEW_CONNECTION_____')
-                if(parsedData.projectName) {
-                    console.log("Project name: "+parsedData.projectName);
-                    this.projectName = parsedData.projectName;
-                    this.events[2].detail.projectName = this.projectName;
-                }
-                if(this.gui != null) {
-                    this.gui.destroy();
-                    this.gui = null;
-                }
-                this.handler.ready = false;
-                Object.keys(this.handler.type).forEach(k => this.handler.type[k] = false)
+		if (parsedData.event == 'connection') {
+			console.log('_____NEW_CONNECTION_____')
+			if(parsedData.projectName) {
+				console.log("Project name: "+parsedData.projectName);
+				this.projectName = parsedData.projectName;
+				this.events[2].detail.projectName = this.projectName;
+			}
+			if(this.gui != null) {
+				this.gui.destroy();
+				this.gui = null;
+			}
+			this.handler.ready = false;
+			Object.keys(this.handler.type).forEach(k => this.handler.type[k] = false)
 
-                this.target.dispatchEvent(this.events[2]);
-                this.send({event: "connection-reply"});
+			this.target.dispatchEvent(this.events[2]);
+			this.send({event: "connection-reply"});
 
-            } else if (parsedData.event == 'set-controller') {
-                console.log('____SET CONTROLLER____')
-                this.handler.type['controller'] = true
-                if(parsedData.name) {
-                    if(this.handler.ready) {
-                        this.addGui.bind(that)
-                        this.addGui(parsedData.name)
-                    } else {
-                        this.gui_prototype[parsedData.name] = {sliders: []}
-                    }
-                }
-            } else if (parsedData.event == 'set-slider') {
-                console.log("Set slider");
-                let precision = 7; // float32
-                parsedData.value = Number(parsedData.value.toFixed(7));
-                parsedData.max = Number(parsedData.max.toFixed(7));
-                parsedData.min = Number(parsedData.min.toFixed(7));
-                parsedData.step = Number(parsedData.step.toFixed(7));
-                let slider = {
-                    controller: parsedData.controller,
-                    name: parsedData.name,
-                    value: parsedData.value,
-                    max: parsedData.max,
-                    min: parsedData.min,
-                    step: parsedData.step
-                }
-                if(this.handler.ready) {
-                    this.addSlider.bind(that)
-                    this.addSlider(slider)
-                } else {
-                    this.gui_prototype[parsedData.controller]['sliders'].push(slider);
-                }
-            } else if (parsedData.event == 'set-select'){
-            } else if (parsedData.event == 'custom') {
-            } else {
-            }
-    }
+		} else if (parsedData.event == 'set-controller') {
+			console.log('____SET CONTROLLER____')
+			this.handler.type['controller'] = true
+			if(parsedData.name) {
+				if(this.handler.ready) {
+					this.addGui.bind(that)
+					this.addGui(parsedData.name)
+				} else {
+					this.gui_prototype[parsedData.name] = {sliders: []}
+				}
+			}
+		} else if (parsedData.event == 'set-slider') {
+			console.log("Set slider");
+			let precision = 7; // float32
+			parsedData.value = Number(parsedData.value.toFixed(7));
+			parsedData.max = Number(parsedData.max.toFixed(7));
+			parsedData.min = Number(parsedData.min.toFixed(7));
+			parsedData.step = Number(parsedData.step.toFixed(7));
+			let slider = {
+				controller: parsedData.controller,
+				name: parsedData.name,
+				value: parsedData.value,
+				max: parsedData.max,
+				min: parsedData.min,
+				step: parsedData.step
+			}
+			if(this.handler.ready) {
+				this.addSlider.bind(that)
+				this.addSlider(slider)
+			} else {
+				this.gui_prototype[parsedData.controller]['sliders'].push(slider);
+			}
+		} else if (parsedData.event == 'set-select'){
+		} else if (parsedData.event == 'custom') {
+		} else {
+		}
+	}
 
-    sliderCallback(value) {
-        let val = Number(value.toFixed(7));
-        let obj = {};
-        obj['event'] = 'slider';
-        obj['controller'] = this.__gui.name;
-        obj['name'] = this.property;
-        obj['value'] = val;
-        let p = window.Bela.control.gui.getPanel({guiId: obj['controller']});
-        let params = window.Bela.control.gui.parameters[p.id][obj['controller']];
-        let index =  Object.keys(params).indexOf(obj['name']);
-        obj['slider'] = index;
-        window.Bela.control.send(obj);
-    }
+	sliderCallback(value) {
+		let val = Number(value.toFixed(7));
+		let obj = {};
+		obj['event'] = 'slider';
+		obj['controller'] = this.__gui.name;
+		obj['name'] = this.property;
+		obj['value'] = val;
+		let p = window.Bela.control.gui.getPanel({guiId: obj['controller']});
+		let params = window.Bela.control.gui.parameters[p.id][obj['controller']];
+		let index =  Object.keys(params).indexOf(obj['name']);
+		obj['slider'] = index;
+		window.Bela.control.send(obj);
+	}
 
-    send(data) {
-        var obj;
-        try {
-                obj = JSON.stringify(data);
-        } catch (e) {
-                console.log('Could not stringify slider json:', e);
-        }
-        if (this.ws.readyState === 1)
-            this.ws.send(obj);
-    }
+	send(data) {
+		var obj;
+		try {
+			obj = JSON.stringify(data);
+		} catch (e) {
+			console.log('Could not stringify slider json:', e);
+		}
+		if (this.ws.readyState === 1)
+			this.ws.send(obj);
+	}
 
-    send(data) {
-        let obj = JSON.stringify(data);
-        if (this.ws.readyState === 1)
-            this.ws.send(obj);
-    }
+	send(data) {
+		let obj = JSON.stringify(data);
+		if (this.ws.readyState === 1)
+			this.ws.send(obj);
+	}
 
-    send(data) {
-        if (this.ws.readyState === 1)
-            this.ws.send(JSON.stringify(data));
-    }
+	send(data) {
+		if (this.ws.readyState === 1)
+			this.ws.send(JSON.stringify(data));
+	}
 }
--- IDE/public/gui/js/GuiCreator.js
+++ IDE/public/gui/js/GuiCreator.js
@@ -1,134 +1,134 @@
 export default class GuiCreator {
 	constructor(containerId='gui-container', dom=document, win=window, wsHandler) {
-        this.wsHandler = wsHandler;
-        this.dom = dom;
-        this.window = win;
-        this.container = dom.getElementById(containerId);
-        if(this.container == null) {
-            this.container = dom.createElement('div');
-            this.container.setAttribute("id", containerId);
-            this.container.setAttribute("class", "flex-container");
-        }
-        this.panel = function(id, element, gui) {
-            this.id = id;
-            this.element = element;
-            this.gui = gui;
-        };
+		this.wsHandler = wsHandler;
+		this.dom = dom;
+		this.window = win;
+		this.container = dom.getElementById(containerId);
+		if(this.container == null) {
+			this.container = dom.createElement('div');
+			this.container.setAttribute("id", containerId);
+			this.container.setAttribute("class", "flex-container");
+		}
+		this.panel = function(id, element, gui) {
+			this.id = id;
+			this.element = element;
+			this.gui = gui;
+		};
 
-        this.panels = [];
-        this.fillPanels = [];
+		this.panels = [];
+		this.fillPanels = [];
 
-        this.parameters = [];
+		this.parameters = [];
 
-        this.panel_props = {
-            autoPlace: false,
-    		resizable : false,
-            width: 300
-    	}
+		this.panel_props = {
+			autoPlace: false,
+			resizable : false,
+			width: 300
+		}
 	}
 
-    newPanel(name) {
-        let pElement = this.dom.createElement("div");
-        let gui = new this.window.dat.GUI({
-            autoPlace: this.panel_props.autoPlace,
-    		resizable : this.panel_props.resizable,
-    		width: this.panel_props.width
-    	})
-        this.panels.push( new this.panel(this.panels.length, pElement, gui) );
+	newPanel(name) {
+		let pElement = this.dom.createElement("div");
+		let gui = new this.window.dat.GUI({
+			autoPlace: this.panel_props.autoPlace,
+			resizable : this.panel_props.resizable,
+			width: this.panel_props.width
+		})
+		this.panels.push( new this.panel(this.panels.length, pElement, gui) );
 
-        let f = gui.addFolder(name);
-        f.open();
-        this.removeCloseButton(gui);
+		let f = gui.addFolder(name);
+		f.open();
+		this.removeCloseButton(gui);
 
-        pElement.appendChild(gui.domElement);
-        pElement.setAttribute("id", "gui-panel-"+this.panels.length);
+		pElement.appendChild(gui.domElement);
+		pElement.setAttribute("id", "gui-panel-"+this.panels.length);
 		pElement.setAttribute("class", "gui-panel");
-        this.container.appendChild(pElement);
-        this.addFillPanels();
-        return this.panels.slice(-1).pop();
-    }
+		this.container.appendChild(pElement);
+		this.addFillPanels();
+		return this.panels.slice(-1).pop();
+	}
 
-    removeCloseButton(guiObj) {
-        let closeButton = guiObj.domElement.getElementsByClassName("close-button")[0];
+	removeCloseButton(guiObj) {
+		let closeButton = guiObj.domElement.getElementsByClassName("close-button")[0];
 		guiObj.domElement.removeChild(closeButton);
-    }
-    removeFillPanels() {
-        for(let fp in this.fillPanels) {
-            this.fillPanels[fp].parentNode.removeChild(this.fillPanels[fp]);
-        }
-        this.fillPanels = [];
-    }
+	}
+	removeFillPanels() {
+		for(let fp in this.fillPanels) {
+			this.fillPanels[fp].parentNode.removeChild(this.fillPanels[fp]);
+		}
+		this.fillPanels = [];
+	}
 
-    addFillPanels() {
-        this.removeFillPanels()
-        let numColumns = Math.floor(window.innerWidth/this.panel_props.width);
-        let numFill = (numColumns - numColumns * ((this.panels.length/numColumns) % 1)) % numColumns;
+	addFillPanels() {
+		this.removeFillPanels()
+		let numColumns = Math.floor(window.innerWidth/this.panel_props.width);
+		let numFill = (numColumns - numColumns * ((this.panels.length/numColumns) % 1)) % numColumns;
 
-        for(let i = 0; i < numFill; i++) {
-            let fillP = document.createElement("div");
-            fillP.setAttribute("class", "fill-panel");
-            fillP.className += " gui-panel";
-            fillP.setAttribute("display", "none");
-            fillP.style.width = this.panel_props.width+"px";
-            this.fillPanels.push(fillP);
-            this.container.appendChild(fillP);
-        }
-    }
+		for(let i = 0; i < numFill; i++) {
+			let fillP = document.createElement("div");
+			fillP.setAttribute("class", "fill-panel");
+			fillP.className += " gui-panel";
+			fillP.setAttribute("display", "none");
+			fillP.style.width = this.panel_props.width+"px";
+			this.fillPanels.push(fillP);
+			this.container.appendChild(fillP);
+		}
+	}
 
-    newSlider({guiId, name, val=0.5, min=0, max=1, step=0.01, panelId, parent}) {
-        if(typeof(guiId) == 'undefined') return null;
+	newSlider({guiId, name, val=0.5, min=0, max=1, step=0.01, panelId, parent}) {
+		if(typeof(guiId) == 'undefined') return null;
 
-        val = val>max ? max : val;
-        val = val<min ? min : val;
+		val = val>max ? max : val;
+		val = val<min ? min : val;
 
-        let p = this.getPanel({guiId: parent});
-        if(p == null)
-            p = this.getPanel({guiId: guiId, panelId: panelId});
-        if(p == null)
-            p = this.newPanel(guiId);
+		let p = this.getPanel({guiId: parent});
+		if(p == null)
+			p = this.getPanel({guiId: guiId, panelId: panelId});
+		if(p == null)
+			p = this.newPanel(guiId);
 
-        let guiF = p.gui.__folders[guiId];
-        if(guiF == null) {
-            guiF = p.gui.addFolder(guiId);
-            guiF.open();
-        }
+		let guiF = p.gui.__folders[guiId];
+		if(guiF == null) {
+			guiF = p.gui.addFolder(guiId);
+			guiF.open();
+		}
 
-        this.parameters[p.id] = this.parameters[p.id] || {};
-        this.parameters[p.id][guiId] = this.parameters[p.id][guiId] || {};
-        this.parameters[p.id][guiId][name] = val;
+		this.parameters[p.id] = this.parameters[p.id] || {};
+		this.parameters[p.id][guiId] = this.parameters[p.id][guiId] || {};
+		this.parameters[p.id][guiId][name] = val;
 
-        let slider = guiF.add(this.parameters[p.id][guiId], name, min, max, step);
-        // disable input
-        slider.domElement.getElementsByTagName("input")[0].disabled = true
-        slider.onChange(this.sliderCallback);
-        return slider;
-    }
+		let slider = guiF.add(this.parameters[p.id][guiId], name, min, max, step);
+		// disable input
+		slider.domElement.getElementsByTagName("input")[0].disabled = true
+		slider.onChange(this.sliderCallback);
+		return slider;
+	}
 
-    getPanel({guiId, panelId}) {
-        let p = this.panels[panelId];
-        if(p == null) {
-            if(typeof(guiId) == 'undefined') return null;
-            let matchP = this.panels.filter(p => { return p.gui.__folders.hasOwnProperty(guiId)});
-            if(matchP.length) {
-                p = matchP[0];
-            }
-        }
-        return p;
-    }
+	getPanel({guiId, panelId}) {
+		let p = this.panels[panelId];
+		if(p == null) {
+			if(typeof(guiId) == 'undefined') return null;
+			let matchP = this.panels.filter(p => { return p.gui.__folders.hasOwnProperty(guiId)});
+			if(matchP.length) {
+				p = matchP[0];
+			}
+		}
+		return p;
+	}
 
-    sliderCallback(value) {
-        let val = Number(value.toFixed(7));
+	sliderCallback(value) {
+		let val = Number(value.toFixed(7));
 
-        let obj = {};
-        obj['controller'] = this.__gui.name;
-        obj['name'] = this.property;
-        obj['value'] = val;
-        return obj;
-    }
+		let obj = {};
+		obj['controller'] = this.__gui.name;
+		obj['name'] = this.property;
+		obj['value'] = val;
+		return obj;
+	}
 
-    destroy() {
-        for (let [key, value] of Object.entries(this.panels)) {
-          value.gui.destroy();
-        }
-    }
+	destroy() {
+		for (let [key, value] of Object.entries(this.panels)) {
+			value.gui.destroy();
+		}
+	}
 }
