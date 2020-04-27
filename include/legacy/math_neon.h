5a26b13c879a88ab03d58ea68cb3ac32c566245c
Giulio Moro <giuliomoro@yahoo.it>
2020-04-27 14:24:10 +0000

    math_neon: added missing legacy file

---
+++ include/legacy/math_neon.h
@@ -0,0 +1,4 @@
+#pragma once
+#warning You included file math_neon.h. This is now deprecated. You should instead use #include <libraries/math_neon/math_neon.h>
+#include <libraries/math_neon/math_neon.h>
+
