66433cf9b3949b3e1afd99853df20e0b585fe146
Giulio Moro <giuliomoro@yahoo.it>
2020-05-13 10:26:02 +0000

    CTAG: conditional spidev paths to support kernel 4.14

--- include/bela_hw_settings.h
+++ include/bela_hw_settings.h
@@ -1,5 +1,12 @@
 #pragma once
 const unsigned int codecI2cBus  = 2;	// Bus for TLV320AIC3104 codec
 const unsigned int codecI2cAddress  = 0x18;	// Address of TLV320AIC3104 codec
+
+#include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 14, 108) // first kernel we shipped with a different location of the spidevs
+const char ctagSpidevGpioCs0[] = "/dev/spidev3.0"; // Path for SPI bus 0
+const char ctagSpidevGpioCs1[] = "/dev/spidev3.1"; // Path for SPI bus 1
+#else // 4.14.108
 const char ctagSpidevGpioCs0[] = "/dev/spidev32766.0"; // Path for SPI bus 0
 const char ctagSpidevGpioCs1[] = "/dev/spidev32766.1"; // Path for SPI bus 1
+#endif // 4.14.108
