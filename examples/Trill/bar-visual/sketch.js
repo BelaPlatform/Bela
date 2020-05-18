a986c3a3dfce1afb8615ad1e86528481aa42bccc
Adan L Benito <adanl.benito@gmail.com>
2020-05-18 16:05:19 +0000

    Updated bar and square visual examples to preload and^Cse Trill js class

--- examples/Trill/bar-visual/sketch.js
+++ examples/Trill/bar-visual/sketch.js
@@ -1,109 +1,40 @@
-class TrillBar {
-	constructor(length, position = [50, 50]) {
-		this.position = position;
-		this.dimensions = [ length, length/5 ];
-		this.touches = {
-			num: 5,
-			scale: 0.75,
-			sizes: new Array(5),
-			locations: new Array(5),
-			activations: [0, 0, 0, 0, 0],
-			// colors: [ 'honeydew', 'azure', 'lavenderblush', 'ivory', 'navajowhite' ]
-			colors: [ 'red', 'blue', 'yellow', 'white', 'cyan' ]
-		}
-	}
-
-	draw() {
-		fill(35);
-        rect(this.position[0], this.position[1], this.dimensions[0], this.dimensions[1], 30);
-
-        for(let t = 0; t < this.touches.num; t++) {
-        	if(this.touchActive(t)) {
-        		this.drawTouch(t);
-        		this.touches.activations[t] = 0;
-        	}
-        }
-	}
-
-	updateTouch(i, location, size) {
-		if(i < this.touches.num) {
-			this.touches.activations[i] = 1;
-			location = constrain(location, 0, 1);
-			this.touches.locations[i] = this.dimensions[0]*location;
-			size = constrain(size, 0, 1);
-			this.touches.sizes[i] = size;
-		}
-	}
-
-	activeTouches() {
-		return this.touches.activations.filter(Boolean).length;
-	}
-
-	touchActive(i) {
-		if(i < this.touches.num)
-			return this.touches.activations[i];
-	}
-
-	setTouchState(i, state) {
-		if(i < this.touches.num)
-			this.touches.activations[t] = Boolean(state);
-	}
-
-	drawTouch(i) {
-		if(i < this.touches.num) {
-			fill(this.touches.colors[i]);
-			let diameter = this.dimensions[1]*this.touches.sizes[i]*this.touches.scale;
-			ellipse(this.position[0] + this.touches.locations[i], this.position[1] + this.dimensions[1]/2, diameter);
-		}
-	}
-
-	changeTouchColor(i, newColor) {
-		this.touches.colors[i] = color(newColor);
-	}
-
-	changeTouchScale(scale) {
-		if(scale <=1 ) {
-			this.touches.scale = scale;
-		}
-	}
-
-	resize(length) {
-		this.dimensions = [length, length/5];
-	}
-}
-
 let spacing;
 let activeTouches = 0;
 let sliderTouchSizeScaler = 0.5;
 let sliderLength = 0.0;
 let belaLogo;
+let trill;
+
+function preload() {
+	belaLogo = loadImage('../images/logo_bar14.png');
+	Bela.control.loadResource("/libraries/Trill/trill.js")
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    frameRate(60);
-    belaLogo = loadImage('../images/logo_bar14.png');
-    sliderLength = width-100;
-    trillBar = new TrillBar(sliderLength*0.60);
+	createCanvas(windowWidth, windowHeight);
+	frameRate(60);
+	sliderLength = width-100;
+	trill = new Trill('bar', sliderLength);
 }
 
 function draw() {
 	background(240);
-    resizeElements();
-    activeTouches = Bela.data.buffers[0];
-    for(let t = 0; t < activeTouches; t++) {
-    	trillBar.updateTouch(t, Bela.data.buffers[1][t], Bela.data.buffers[2][t]);
-    }
-    trillBar.draw();
+	resizeElements();
+	activeTouches = Bela.data.buffers[0];
+	for(let t = 0; t < activeTouches; t++) {
+		trill.updateTouch(t, Bela.data.buffers[1][t], Bela.data.buffers[2][t]);
+	}
+	trill.draw();
 
-    image(belaLogo, width-170, height-70, 120, 50);
+	image(belaLogo, width-170, height-70, 120, 50);
 }
 
 function resizeElements() {
 	sliderLength = width-100;
-	trillBar.resize(sliderLength);
+	trill.resize(sliderLength);
 }
 
 function windowResized() {
 	resizeElements();
-    resizeCanvas(windowWidth, windowHeight);
+	resizeCanvas(windowWidth, windowHeight);
 }
--- examples/Trill/square-visual/sketch.js
+++ examples/Trill/square-visual/sketch.js
@@ -1,108 +1,46 @@
-class TrillSquare {
-	constructor(width, position = [50, 50]) {
-		this.position = position;
-		this.width = width;
-		this.touch = {
-			scale: 0.25,
-			size: 0,
-			location: [null, null],
-			active: 0,
-			color: 'ivory'
-		}
-	}
-
-	draw() {
-		fill(0);
-		rect(this.position[0], this.position[1], this.width, this.width, 20);
-		if(this.touchActive()) {
-			this.drawTouch();
-			this.touch.active = 0;
-		}
-	}
-
-	touchActive() {
-		return this.touch.active;
-	}
-
-	updateTouch(location, size) {
-		this.touch.active = 1;
-		location[0] = constrain(location[0], 0, 1);
-		location[1] = constrain((-location[1] + 1), 0, 1);
-		this.touch.location = [this.width*location[0], this.width*location[1]];
-		size = constrain(size, 0, 1);
-		this.touch.size = size;
-	}
-
-	setTouchState(state) {
-		this.touch.active = Boolean(state);
-	}
-
-	drawTouch(i) {
-		fill(this.touch.color);
-		let diameter = this.width*this.touch.size*this.touch.scale;
-		ellipse(this.position[0] + this.touch.location[0], this.position[1] + this.touch.location[1], diameter);
-	}
-
-	changeTouchColor(newColor) {
-		this.touch.color = color(newColor);
-	}
-
-	changeTouchScale(scale) {
-		if(scale <=1 ) {
-			this.touch.scale = scale;
-		}
-	}
-
-	resize(width, start) {
-		this.width = width;
-		this.position[0] = windowWidth/2-width/2;
-		this.position[1] = start;
-	}
-}
-
-
 let spacing;
 let startPoint;
-
-let numTouches = 0;
 let touchSize = 0;
 let touchPosition = [0, 0];
+let trill;
 
+function preload() {
+	belaLogo = loadImage('../images/logo_bar14.png');
+	Bela.control.loadResource("/libraries/Trill/trill.js")
+}
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    spacing=height/20;
-    startPoint=(width/2)-spacing*7;
-    rectWidth = height*0.7;
-    rectStart = height*0.1;
+	createCanvas(windowWidth, windowHeight);
+	spacing=height/20;
+	startPoint=(width/2)-spacing*7;
+	rectWidth = height*0.7;
+	rectStart = height*0.1;
 
-    trill = new TrillSquare(rectWidth, [width/2-rectWidth/2, rectStart]);
-
-    belaLogo = loadImage('../images/logo_bar14.png');
+	trill = new Trill('square', rectWidth, [width/2-rectWidth/2, rectStart]);
 }
 
 function draw() {
-    background(240);
-    resizeElements();
+	background(240);
+	resizeElements();
 
-    if(typeof Bela.data.buffers[0] != 'undefined')
-    	touchPosition = Bela.data.buffers[0];
+	if(typeof Bela.data.buffers[0] != 'undefined')
+		touchPosition = Bela.data.buffers[0];
 
-    if(typeof Bela.data.buffers[1] != 'undefined')
-    	touchSize = Bela.data.buffers[1][0];
-    print(touchSize);
-    trill.updateTouch(touchPosition, touchSize);
-    trill.draw();
-    image(belaLogo,width-170, height-70,120,50);
+	if(typeof Bela.data.buffers[1] != 'undefined')
+		touchSize = Bela.data.buffers[1][0];
+
+	trill.updateTouch(0, touchPosition, touchSize);
+	trill.draw();
+	image(belaLogo,width-170, height-70,120,50);
 }
 
 function resizeElements() {
-    rectWidth = height*0.7;
-    rectStart = height*0.1;
+	rectWidth = height*0.7;
+	rectStart = height*0.1;
 }
 
 function windowResized() {
 	resizeElements();
-    trill.resize(rectWidth, rectStart);
-    resizeCanvas(windowWidth, windowHeight);
+	trill.resize(rectWidth, rectStart);
+	resizeCanvas(windowWidth, windowHeight);
 }
