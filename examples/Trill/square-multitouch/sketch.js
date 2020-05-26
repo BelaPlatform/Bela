543a7d6c5fef8212265ba4d6088f709ecc4b6fea
Giulio Moro <giuliomoro@yahoo.it>
2020-05-26 09:56:40 +0000

    Trill: square-multitouch: removed unneeded global variables, fixed resizing

--- examples/Trill/square-multitouch/sketch.js
+++ examples/Trill/square-multitouch/sketch.js
@@ -1,9 +1,5 @@
-let spacing;
-let startPoint;
-let touchSize = 0;
-let touchVerticalPosition = [0, 0];
-let touchHorizontalPosition = [0, 0];
 let trill;
+let belaLogo;
 
 function preload() {
 	belaLogo = loadImage('../images/logo_bar14.png');
@@ -12,20 +8,14 @@ function preload() {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	spacing=height/20;
-	startPoint=(width/2)-spacing*7;
-	rectWidth = height*0.7;
-	rectStart = height*0.1;
-
-	trill = new Trill('square', rectWidth, [width/2-rectWidth/2, rectStart]);
+	trill = new Trill('square');
+	windowResized();
 }
 
 function draw() {
 	background(240);
-	resizeElements();
-		
-	activeVTouches = Bela.data.buffers[0];
-	activeHTouches = Bela.data.buffers[1];
+	let activeVTouches = Bela.data.buffers[0];
+	let activeHTouches = Bela.data.buffers[1];
 	for(let t = 0; t < activeVTouches; t++) {
 		for(let u = 0; u < activeHTouches; u++) {
 			trill.updateTouch(t*activeVTouches + u, [Bela.data.buffers[2][u], Bela.data.buffers[3][t]], Bela.data.buffers[4][t]);
@@ -36,13 +26,8 @@ function draw() {
 	image(belaLogo,width-170, height-70,120,50);
 }
 
-function resizeElements() {
-	rectWidth = height*0.7;
-	rectStart = height*0.1;
-}
-
 function windowResized() {
-	resizeElements();
-	trill.resize(rectWidth, rectStart);
 	resizeCanvas(windowWidth, windowHeight);
+	trill.resize(height * 0.7);
+	trill.position = [width / 2, height / 2];
 }
