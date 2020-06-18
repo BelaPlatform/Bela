BelaPlatform/Bela

commit 1b4568e09b2c0c0c67c5357fb7f91ba0af348590

FEAT: Latest IDE front-end refactor

IDE/public/js/bundle.js
@@ -1517,11 +1517,11 @@ var DocumentationView = function (_View) {
 		_this.on('init', _this.init);
 
 		_this.on('open', function (id) {
-			_this.closeAll();
-			$('#' + id).prop('checked', 'checked');
-			$('#' + id).parent().parent().siblings('input').prop('checked', 'checked');
-			var offset = $('#' + id).siblings('label').position().top + $('#docTab').scrollTop();
-			if (offset) $('#docTab').scrollTop(offset);
+			// this.closeAll();
+			// $('#'+id).prop('checked', 'checked');
+			// $('#'+id).parent().parent().siblings('input').prop('checked', 'checked');
+			// var offset = $('#'+id).siblings('label').position().top + $('#docTab').scrollTop();
+			// if (offset) $('#docTab').scrollTop(offset);
 		});
 		return _this;
 	}
@@ -1634,7 +1634,7 @@ var DocumentationView = function (_View) {
 	}, {
 		key: 'closeAll',
 		value: function closeAll() {
-			$('#docsParent').find('input:checked').prop('checked', '');
+			// $('#docsParent').find('input:checked').prop('checked', '');
 		}
 	}]);
 
@@ -1649,16 +1649,16 @@ function createlifrommemberdef($xml, id, emitter, type) {
 	emitter.emit('add-link', { name: name, id: id }, type);
 
 	var li = $('<li></li>');
-	li.append($('<input></input>').prop('type', 'checkbox').addClass('docs').prop('id', id));
-	li.append($('<label></label>').prop('for', id).addClass('docSectionHeader').addClass('sub').html(name));
+	// li.append($('<input></input>').prop('type', 'checkbox').addClass('docs').prop('id', id));
+	// li.append($('<label></label>').prop('for', id).addClass('docSectionHeader').addClass('sub').html(name));
 
 	var content = $('<div></div>');
 
 	// title
-	content.append($('<h2></h2>').html($xml.find('definition').html() + $xml.find('argsstring').html()));
+	content.append($('<h2></h2>').html($xml.find('definition').html() + $xml.find('argsstring').html() + "<hr />"));
 
 	// subtitle
-	content.append($('<h3></h3>').html($xml.find('briefdescription > para').html() || ''));
+	content.append($('<p></p>').html($xml.find('briefdescription > para').html() || ''));
 
 	// main text
 	$xml.find('detaileddescription > para').each(function () {
@@ -1689,8 +1689,8 @@ function createlifromxml($xml, id, filename, emitter, type) {
 	emitter.emit('add-link', { name: name, id: id }, type);
 
 	var li = $('<li></li>');
-	li.append($('<input></input>').prop('type', 'checkbox').addClass('docs').prop('id', id));
-	li.append($('<label></label>').prop('for', id).addClass('docSectionHeader').addClass('sub').html(name));
+	// li.append($('<input></input>').prop('type', 'checkbox').addClass('docs').prop('id', id));
+	// li.append($('<label></label>').prop('for', id).addClass('docSectionHeader').addClass('sub').html(name));
 
 	var content = $('<div></div>');
 
@@ -2870,7 +2870,6 @@ var ProjectView = function (_View) {
 	function ProjectView(className, models) {
 		_classCallCheck(this, ProjectView);
 
-		//this.exampleChanged = false;
 		var _this = _possibleConstructorReturn(this, (ProjectView.__proto__ || Object.getPrototypeOf(ProjectView)).call(this, className, models));
 
 		_this.on('example-changed', function () {
@@ -2892,8 +2891,6 @@ var ProjectView = function (_View) {
 				popup.exampleChanged(function () {
 					_this2.emit('message', 'project-event', { func: $element.data().func, currentProject: $element.val() });
 				}, undefined, 0, function () {
-					$element.find('option').filter(':selected').attr('selected', '');
-					$element.val($('#projects > option:first').val());
 					_this2.exampleChanged = true;
 				});
 				return;
@@ -3020,16 +3017,13 @@ var ProjectView = function (_View) {
 		key: '_projectList',
 		value: function _projectList(projects, data) {
 
-			var $projects = $('#projects');
-			$projects.empty();
+			var $projects = $('[data-projects-select]');
 
-			// add an empty option to menu and select it
-			var opt = $('<option></option>').attr({ 'value': '', 'selected': 'selected' }).html('--Projects--').appendTo($projects);
-
+			// var option = $('<ul></ul>').addClass('dropdown-content').attr('data-dropdown', 'project');
 			// fill project menu with projects
 			for (var i = 0; i < projects.length; i++) {
 				if (projects[i] && projects[i] !== 'undefined' && projects[i] !== 'exampleTempProject' && projects[i][0] !== '.') {
-					var opt = $('<option></option>').attr('value', projects[i]).html(projects[i]).appendTo($projects);
+					$('<option></option>').addClass('projectManager').val(projects[i]).attr('data-func', 'openProject').html(projects[i]).appendTo($projects);
 				}
 			}
 
@@ -3513,7 +3507,9 @@ var SettingsView = function (_View) {
 					this.setAudioExpander(key, data[key]);
 					continue;
 				} else if (key === 'audioExpander') {
-					if (data[key] == 1) $('#audioExpanderTable').css('display', 'table');else $('#audioExpanderTable').css('display', 'none');
+					if (data[key] == 1) $('#audioExpanderTable').css('display', 'table');
+					// else
+					// 	$('#audioExpanderTable').css('display', 'none');
 				}
 
 				var el = this.$elements.filterByData('key', key);
@@ -3559,10 +3555,10 @@ var SettingsView = function (_View) {
 		value: function useAudioExpander(func, key, val) {
 
 			if (val == 1) {
-				$('#audioExpanderTable').css('display', 'table');
+				// $('#audioExpanderTable').css('display', 'table');
 				this.setCLArg('setCLArg', key, val);
 			} else {
-				$('#audioExpanderTable').css('display', 'none');
+				// $('#audioExpanderTable').css('display', 'none');
 				// clear channel picker
 				$('.audioExpanderCheck').prop('checked', false);
 				this.emit('project-settings', { func: 'setCLArgs', args: [{ key: '-Y', value: '' }, { key: '-Z', value: '' }, { key: key, value: val }] });
@@ -3780,23 +3776,34 @@ var TabView = function (_View) {
 			content: [{
 				type: 'column',
 				content: [{
-					type: 'row',
+					type: 'component',
+					componentName: 'Editor'
+				}, {
+					type: 'column',
+					height: 40,
+					componentName: 'Lower',
 					content: [{
 						type: 'component',
-						componentName: 'Editor'
+						componentName: 'Tools',
+						height: 25
+					}, {
+						type: 'component',
+						componentName: 'Console'
 					}]
-				}, {
-					type: 'component',
-					componentName: 'Console',
-					height: 25
 				}]
 			}]
 		});
 		layout.registerComponent('Editor', function (container, componentState) {
-			container.getElement().append($('[data-layout-editor]'));
+			container.getElement().append($('[data-upper]'));
+		});
+		layout.registerComponent('Tools', function (container, componentState) {
+			container.getElement().append($('[data-toolbar]'));
 		});
 		layout.registerComponent('Console', function (container, componentState) {
-			container.getElement().append($('[data-layout-console]'));
+			container.getElement().append($('[data-console]'));
+		});
+		layout.registerComponent('Lower', function (container, componentState) {
+			container.getElement().append($('[data-lower]'));
 		});
 
 		layout.init();
@@ -3811,37 +3818,6 @@ var TabView = function (_View) {
 
 		return _this;
 	}
-
-	// openTabs(){
-	// 	$('#editor').css('right', '500px');
-	// 	$('#top-line').css('margin-right', '500px');
-	// 	$('#right').css('left', window.innerWidth - 500 + 'px');
-	// 	_tabsOpen = true;
-	// 	this.emit('change');
-	// 	$('#tab-0').addClass('open');
-	//
-	// 	// fix pd patch
-	// 	$('#pd-svg-parent').css({
-	// 		'max-width'	: $('#editor').width()+'px',
-	// 		'max-height': $('#editor').height()+'px'
-	// 	});
-	// }
-	//
-	// closeTabs(){
-	// 	$('#editor').css('right', '60px');
-	// 	$('#top-line').css('margin-right', '60px');
-	// 	$('#right').css('left', window.innerWidth - 60 + 'px');
-	// 	_tabsOpen = false;
-	// 	this.emit('change');
-	// 	$('#tab-0').removeClass('open');
-	//
-	// 	// fix pd patch
-	// 	$('#pd-svg-parent').css({
-	// 		'max-width'	: $('#editor').width()+'px',
-	// 		'max-height': $('#editor').height()+'px'
-	// 	});
-	//
-	// }
 
 	_createClass(TabView, [{
 		key: '_boardString',
@@ -3900,43 +3876,62 @@ var ToolbarView = function (_View) {
 		});
 
 		_this.on('disconnected', function () {
-			$('#run').removeClass('running-button').removeClass('building-button');
+			// $('#run').removeClass('running-button').removeClass('building-button');
+			$('[data-toolbar-run]').removeClass('running-button').removeClass('building-button');
 		});
 
-		$('#run').mouseover(function () {
-			$('#control-text-1').html('<p>Run</p>');
+		// $('#run')
+		$('[data-toolbar-run]').mouseover(function () {
+			// $('#control-text-1').html('<p>Run</p>');
+			$('[data-toolbar-controltext1]').html('<p>Run</p>');
 		}).mouseout(function () {
-			$('#control-text-1').html('');
+			// $('#control-text-1').html('');
+			$('[data-toolbar-controltext1]').html('');
 		});
 
-		$('#stop').mouseover(function () {
-			$('#control-text-1').html('<p>Stop</p>');
+		// $('#stop')
+		$('[data-toolbar-stop]').mouseover(function () {
+			// $('#control-text-1').html('<p>Stop</p>');
+			$('[data-toolbar-controltext1]').html('<p>Stop</p>');
 		}).mouseout(function () {
-			$('#control-text-1').html('');
+			// $('#control-text-1').html('');
+			$('[data-toolbar-controltext1]').html('');
 		});
 
-		$('#new-tab').mouseover(function () {
-			$('#control-text-2').html('<p>New Tab</p>');
+		// $('#new-tab')
+		$('[data-toolbar-newtab]').mouseover(function () {
+			// $('#control-text-2').html('<p>New Tab</p>');
+			$('[data-toolbar-controltext2]').html('<p>New Tab</p>');
 		}).mouseout(function () {
-			$('#control-text-2').html('');
+			// $('#control-text-2').html('');
+			$('[data-toolbar-controltext2]').html('');
 		});
 
-		$('#download').mouseover(function () {
-			$('#control-text-2').html('<p>Download</p>');
+		// $('#download')
+		$('[data-toolbar-download]').mouseover(function () {
+			// $('#control-text-2').html('<p>Download</p>');
+			$('[data-toolbar-controltext2]').html('<p>Download</p>');
 		}).mouseout(function () {
-			$('#control-text-2').html('');
+			// $('#control-text-2').html('');
+			$('[data-toolbar-controltext2]').html('');
 		});
 
-		$('#console').mouseover(function () {
-			$('#control-text-3').html('<p>Clear console</p>');
+		// $('#console')
+		$('[data-toolbar-console]').mouseover(function () {
+			// $('#control-text-3').html('<p>Clear console</p>');
+			$('[data-toolbar-controltext3]').html('<p>Clear console</p>');
 		}).mouseout(function () {
-			$('#control-text-3').html('');
+			// $('#control-text-3').html('');
+			$('[data-toolbar-controltext3]').html('');
 		});
 
-		$('#scope').mouseover(function () {
-			$('#control-text-3').html('<p>Open scope</p>');
+		// $('#scope')
+		$('[data-toolbar-scope]').mouseover(function () {
+			// $('#control-text-3').html('<p>Open scope</p>');
+			$('[data-toolbar-controltext3]').html('<p>Open scope</p>');
 		}).mouseout(function () {
-			$('#control-text-3').html('');
+			// $('#control-text-3').html('');
+			$('[data-toolbar-controltext3]').html('');
 		});
 		return _this;
 	}
@@ -3950,6 +3945,7 @@ var ToolbarView = function (_View) {
 			var func = $element.data().func;
 			if (func && this[func]) {
 				this[func](func);
+				console.log($element);
 			}
 		}
 	}, {
@@ -3974,11 +3970,15 @@ var ToolbarView = function (_View) {
 		key: '__running',
 		value: function __running(status) {
 			if (status) {
-				$('#run').removeClass('building-button').addClass('running-button');
+				// $('#run').removeClass('building-button').addClass('running-button');
+				$('[data-toolbar-run]').removeClass('building-button').addClass('running-button');
 			} else {
-				$('#run').removeClass('running-button');
-				$('#bela-cpu').html('CPU: --').css('color', 'black');
-				$('#msw-cpu').html('MSW: --').css('color', 'black');
+				// $('#run').removeClass('running-button');
+				$('[data-toolbar-run]').removeClass('running-button');
+				// $('#bela-cpu').html('CPU: --').css('color', 'black');
+				$('[data-toolbar-bela-cpu]').html('CPU: --').css('color', 'black');
+				// $('#msw-cpu').html('MSW: --').css('color', 'black');
+				$('[data-toolbar-msw-cpu]').html('MSW: --').css('color', 'black');
 				modeswitches = 0;
 			}
 		}
@@ -3986,16 +3986,19 @@ var ToolbarView = function (_View) {
 		key: '__building',
 		value: function __building(status) {
 			if (status) {
-				$('#run').removeClass('running-button').addClass('building-button');
+				// $('#run').removeClass('running-button').addClass('building-button');
+				$('[data-toolbar-run]').removeClass('running-button').addClass('building-button');
 			} else {
-				$('#run').removeClass('building-button');
+				// $('#run').removeClass('building-button');
+				$('[data-toolbar-run]').removeClass('building-button');
 			}
 		}
 	}, {
 		key: '__checkingSyntax',
 		value: function __checkingSyntax(status) {
 			if (status) {
-				$('#status').css('background', 'url("images/icons/status_wait.png")').prop('title', 'checking syntax...');
+				// $('#status').css('background', 'url("images/icons/status_wait.png")').prop('title', 'checking syntax...');
+				$('[data-toolbar-status]').addClass('pending').removeClass('ok').removeClass('stop').prop('title', 'checking syntax&hellip;');
 			} else {
 				//this.syntaxTimeout = setTimeout(() => $('#status').css('background', 'url("images/toolbar.png") -140px 35px'), 10);
 			}
@@ -4003,11 +4006,13 @@ var ToolbarView = function (_View) {
 	}, {
 		key: '__allErrors',
 		value: function __allErrors(errors) {
-			//if (this.syntaxTimeout) clearTimeout(this.syntaxTimeout); 
+			//if (this.syntaxTimeout) clearTimeout(this.syntaxTimeout);
 			if (errors.length) {
-				$('#status').css('background', 'url("images/icons/status_stop.png")').prop('title', 'syntax errors found');
+				// $('#status').css('background', 'url("images/icons/status_stop.png")').prop('title', 'syntax errors found');
+				$('[data-toolbar-status]').removeClass('pending').removeClass('ok').addClass('stop').prop('title', 'syntax errors found');
 			} else {
-				$('#status').css('background', 'url("images/icons/status_ok.png")').prop('title', 'syntax check clear');
+				// $('#status').css('background', 'url("images/icons/status_ok.png")').prop('title', 'syntax check clear');
+				$('[data-toolbar-status]').removeClass('pending').addClass('ok').removeClass('stop').prop('title', 'syntax check clear');
 			}
 		}
 	}, {
@@ -4076,27 +4081,36 @@ var ToolbarView = function (_View) {
 			}
 
 			//	$('#ide-cpu').html('IDE: '+(ide*rootCPU).toFixed(1)+'%');
-			$('#bela-cpu').html('CPU: ' + (bela ? bela.toFixed(1) + '%' : '--'));
+			// $('#bela-cpu').html('CPU: '+( bela ? bela.toFixed(1)+'%' : '--'));
+			$('[data-toolbar-bela-cpu]').html('CPU: ' + (bela ? bela.toFixed(1) + '%' : '--'));
 
 			//	if (bela && (ide*rootCPU + bela) > 80){
 			if (bela && bela > 80) {
-				$('#bela-cpu').css('color', 'red');
+				// $('#bela-cpu').css('color', 'red');
+				$('[data-toolbar-bela-cpu]').css('color', 'red');
 			} else {
-				$('#bela-cpu').css('color', 'black');
+				// $('#bela-cpu').css('color', 'black');
+				$('[data-toolbar-bela-cpu]').css('color', 'black');
 			}
 		}
 	}, {
 		key: '_cpuMonitoring',
 		value: function _cpuMonitoring(value) {
-			if (parseInt(value)) $('#bela-cpu').css('visibility', 'visible');else $('#bela-cpu').css('visibility', 'hidden');
+			if (parseInt(value))
+				// $('#bela-cpu').css('visibility', 'visible');
+				$('[data-toolbar-bela-cpu]').css('visibility', 'visible');else
+				// $('#bela-cpu').css('visibility', 'hidden');
+				$('[data-toolbar-bela-cpu]').css('visibility', 'hidden');
 		}
 	}, {
 		key: 'mode_switches',
 		value: function mode_switches(value) {
-			$('#msw-cpu').html('MSW: ' + value);
+			// $('#msw-cpu').html('MSW: '+value);
+			$('[data-toolbar-msw-cpu]').html('MSW: ' + value);
 			if (value > modeswitches) {
 				this.emit('mode-switch-warning', value);
-				$('#msw-cpu').css('color', 'red');
+				// $('#msw-cpu').css('color', 'red');
+				$('[data-toolbar-msw-cpu]').css('color', 'red');
 			}
 			modeswitches = value;
 		}
@@ -5030,9 +5044,9 @@ module.exports = parser;
 },{"./CircularBuffer":2}],18:[function(require,module,exports){
 'use strict';
 
-var _overlay = $('#overlay');
-var parent = $('#popup');
-var content = $('#popup-content');
+var _overlay = $('[data-overlay]');
+var parent = $('[data-popup]');
+var content = $('[data-popup-content]');
 var titleEl = parent.find('h1');
 var subEl = parent.find('p');
 var _formEl = parent.find('form');

dev/src/Views/ToolbarView.js
@@ -7,116 +7,145 @@ var NORMAL_MSW = 1;
 var nameIndex, CPUIndex, rootName, IRQName;
 
 class ToolbarView extends View {
-	
+
 	constructor(className, models){
 		super(className, models);
 
 		this.$elements.on('click', (e) => this.buttonClicked($(e.currentTarget), e));
-		
+
 		this.on('disconnected', () => {
-			$('#run').removeClass('running-button').removeClass('building-button');
+			// $('#run').removeClass('running-button').removeClass('building-button');
+      $('[data-toolbar-run]').removeClass('running-button').removeClass('building-button');
 		});
-		
-		$('#run')
+
+		// $('#run')
+    $('[data-toolbar-run]')
 			.mouseover(function() {
-				$('#control-text-1').html('<p>Run</p>');
+        // $('#control-text-1').html('<p>Run</p>');
+				$('[data-toolbar-controltext1]').html('<p>Run</p>');
 			})
 			.mouseout(function() {
-				$('#control-text-1').html('');
-			});
-		
-		$('#stop')
-			.mouseover(function() {
-				$('#control-text-1').html('<p>Stop</p>');
-			})
-			.mouseout(function() {
-				$('#control-text-1').html('');
+        // $('#control-text-1').html('');
+				$('[data-toolbar-controltext1]').html('');
 			});
 
-		$('#new-tab')
+    // $('#stop')
+		$('[data-toolbar-stop]')
 			.mouseover(function() {
-				$('#control-text-2').html('<p>New Tab</p>');
+        // $('#control-text-1').html('<p>Stop</p>');
+				$('[data-toolbar-controltext1]').html('<p>Stop</p>');
 			})
 			.mouseout(function() {
-				$('#control-text-2').html('');
-			});
-		
-		$('#download')
-			.mouseover(function() {
-				$('#control-text-2').html('<p>Download</p>');
-			})
-			.mouseout(function() {
-				$('#control-text-2').html('');
+        // $('#control-text-1').html('');
+				$('[data-toolbar-controltext1]').html('');
 			});
 
-		$('#console')
+    // $('#new-tab')
+		$('[data-toolbar-newtab]')
 			.mouseover(function() {
-				$('#control-text-3').html('<p>Clear console</p>');
+        // $('#control-text-2').html('<p>New Tab</p>');
+				$('[data-toolbar-controltext2]').html('<p>New Tab</p>');
 			})
 			.mouseout(function() {
-				$('#control-text-3').html('');
+        // $('#control-text-2').html('');
+				$('[data-toolbar-controltext2]').html('');
 			});
-		
-		$('#scope')
+
+    // $('#download')
+		$('[data-toolbar-download]')
 			.mouseover(function() {
-				$('#control-text-3').html('<p>Open scope</p>');
+        // $('#control-text-2').html('<p>Download</p>');
+				$('[data-toolbar-controltext2]').html('<p>Download</p>');
 			})
 			.mouseout(function() {
-				$('#control-text-3').html('');
+        // $('#control-text-2').html('');
+				$('[data-toolbar-controltext2]').html('');
+			});
+
+    // $('#console')
+		$('[data-toolbar-console]')
+			.mouseover(function() {
+        // $('#control-text-3').html('<p>Clear console</p>');
+				$('[data-toolbar-controltext3]').html('<p>Clear console</p>');
+			})
+			.mouseout(function() {
+        // $('#control-text-3').html('');
+				$('[data-toolbar-controltext3]').html('');
+			});
+
+    // $('#scope')
+		$('[data-toolbar-scope]')
+			.mouseover(function() {
+        // $('#control-text-3').html('<p>Open scope</p>');
+				$('[data-toolbar-controltext3]').html('<p>Open scope</p>');
+			})
+			.mouseout(function() {
+        // $('#control-text-3').html('');
+				$('[data-toolbar-controltext3]').html('');
 			});
 	}
-	
+
 	// UI events
 	buttonClicked($element, e){
 		var func = $element.data().func;
 		if (func && this[func]){
 			this[func](func);
+      console.log($element);
 		}
 	}
-	
+
 	run(func){
 		this.emit('process-event', func);
 	}
-	
+
 	stop(func){
 		this.emit('process-event', func);
 	}
-	
+
 	clearConsole(){
 		this.emit('clear-console');
 	}
-	
+
 	// model events
 	__running(status){
 		if (status){
-			$('#run').removeClass('building-button').addClass('running-button');
+      // $('#run').removeClass('building-button').addClass('running-button');
+			$('[data-toolbar-run]').removeClass('building-button').addClass('running-button');
 		} else {
-			$('#run').removeClass('running-button');
-			$('#bela-cpu').html('CPU: --').css('color', 'black');
-			$('#msw-cpu').html('MSW: --').css('color', 'black');
+      // $('#run').removeClass('running-button');
+			$('[data-toolbar-run]').removeClass('running-button');
+      // $('#bela-cpu').html('CPU: --').css('color', 'black');
+			$('[data-toolbar-bela-cpu]').html('CPU: --').css('color', 'black');
+      // $('#msw-cpu').html('MSW: --').css('color', 'black');
+			$('[data-toolbar-msw-cpu]').html('MSW: --').css('color', 'black');
 			modeswitches = 0;
 		}
 	}
 	__building(status){
 		if (status){
-			$('#run').removeClass('running-button').addClass('building-button');
+      // $('#run').removeClass('running-button').addClass('building-button');
+			$('[data-toolbar-run]').removeClass('running-button').addClass('building-button');
 		} else {
-			$('#run').removeClass('building-button');
+      // $('#run').removeClass('building-button');
+			$('[data-toolbar-run]').removeClass('building-button');
 		}
 	}
 	__checkingSyntax(status){
 		if (status){
-			$('#status').css('background', 'url("images/icons/status_wait.png")').prop('title', 'checking syntax...');
+      // $('#status').css('background', 'url("images/icons/status_wait.png")').prop('title', 'checking syntax...');
+			$('[data-toolbar-status]').addClass('pending').removeClass('ok').removeClass('stop').prop('title', 'checking syntax&hellip;');
 		} else {
 			//this.syntaxTimeout = setTimeout(() => $('#status').css('background', 'url("images/toolbar.png") -140px 35px'), 10);
 		}
 	}
 	__allErrors(errors){
-		//if (this.syntaxTimeout) clearTimeout(this.syntaxTimeout); 
+		//if (this.syntaxTimeout) clearTimeout(this.syntaxTimeout);
 		if (errors.length){
-			$('#status').css('background', 'url("images/icons/status_stop.png")').prop('title', 'syntax errors found'); 
+      // $('#status').css('background', 'url("images/icons/status_stop.png")').prop('title', 'syntax errors found');
+			$('[data-toolbar-status]').removeClass('pending').removeClass('ok').addClass('stop').prop('title', 'syntax errors found');
 		} else {
-			$('#status').css('background', 'url("images/icons/status_ok.png")').prop('title', 'syntax check clear');
+      // $('#status').css('background', 'url("images/icons/status_ok.png")').prop('title', 'syntax check clear');
+			$('[data-toolbar-status]').removeClass('pending').addClass('ok').removeClass('stop').prop('title', 'syntax check clear');
 		}
 	}
 
@@ -140,7 +169,7 @@ class ToolbarView extends View {
 		var bela = 0, rootCPU = 1;
 
 		if (data.bela != 0 && data.bela !== undefined){
-		
+
 			// extract the data from the output
 			var lines = data.bela.split('\n');
 			var taskData = [];
@@ -153,7 +182,7 @@ class ToolbarView extends View {
 					}
 				}
 			}
-			
+
 			var output = [];
 			for (var j=0; j<taskData.length; j++){
 				if (taskData[j].length){
@@ -184,29 +213,36 @@ class ToolbarView extends View {
 		}
 
 	//	$('#ide-cpu').html('IDE: '+(ide*rootCPU).toFixed(1)+'%');
-		$('#bela-cpu').html('CPU: '+( bela ? bela.toFixed(1)+'%' : '--'));
-		
+    // $('#bela-cpu').html('CPU: '+( bela ? bela.toFixed(1)+'%' : '--'));
+		$('[data-toolbar-bela-cpu]').html('CPU: '+( bela ? bela.toFixed(1)+'%' : '--'));
+
 	//	if (bela && (ide*rootCPU + bela) > 80){
 		if (bela && bela > 80) {
-			$('#bela-cpu').css('color', 'red');
+      // $('#bela-cpu').css('color', 'red');
+			$('[data-toolbar-bela-cpu]').css('color', 'red');
 		} else {
-			$('#bela-cpu').css('color', 'black');
+      // $('#bela-cpu').css('color', 'black');
+			$('[data-toolbar-bela-cpu]').css('color', 'black');
 		}
-		
+
 	}
-	
+
 	_cpuMonitoring(value){
 		if (parseInt(value))
-			$('#bela-cpu').css('visibility', 'visible');
+      // $('#bela-cpu').css('visibility', 'visible');
+			$('[data-toolbar-bela-cpu]').css('visibility', 'visible');
 		else
-			$('#bela-cpu').css('visibility', 'hidden');
+      // $('#bela-cpu').css('visibility', 'hidden');
+			$('[data-toolbar-bela-cpu]').css('visibility', 'hidden');
 	}
-	
+
 	mode_switches(value){
-		$('#msw-cpu').html('MSW: '+value);
+    // $('#msw-cpu').html('MSW: '+value);
+		$('[data-toolbar-msw-cpu]').html('MSW: '+value);
 		if (value > modeswitches){
 			this.emit('mode-switch-warning', value);
-			$('#msw-cpu').css('color', 'red');
+      // $('#msw-cpu').css('color', 'red');
+			$('[data-toolbar-msw-cpu]').css('color', 'red');
 		}
 		modeswitches = value;
 	}

IDE/public/diagramStyle.css
@@ -7,15 +7,14 @@
 }
 
 #container {
-    align: left;
     overflow: visible;
 }
 
 #image {
     width: 350px;
     text-align: left;
-    margin: 10px auto 40px;
-    padding-left: 30px;
+    margin: 0;
+    padding-left: 0;
     overflow: visible;
 }
 

IDE/public/images/icons/syntax_pending.svg
@@ -1,0 +1,13 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="34.723px" height="34.723px" viewBox="0 0 34.723 34.723" enable-background="new 0 0 34.723 34.723" xml:space="preserve">
+<path fill="#FFFF00" d="M34.635,17.356c0,9.593-7.771,17.367-17.361,17.367c-9.587,0-17.358-7.772-17.358-17.367
+	C-0.086,7.774,7.687,0,17.274,0C26.865,0,34.635,7.774,34.635,17.356z"/>
+<polyline display="none" fill="none" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+	27.222,12.549 15.161,24.609 15.127,24.578 8.856,18.307 "/>
+<circle fill="#FFFFFF" cx="17.206" cy="21.064" r="1.685"/>
+<circle fill="#FFFFFF" cx="25.205" cy="21.064" r="1.686"/>
+<circle fill="#FFFFFF" cx="9.206" cy="21.064" r="1.685"/>
+</svg>

IDE/public/images/icons/syntax_stop.svg
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Generator: Adobe Illustrator 16.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="34.723px" height="34.723px" viewBox="0 0 34.723 34.723" enable-background="new 0 0 34.723 34.723" xml:space="preserve">
+<path fill="#FF0000" d="M34.635,17.356c0,9.593-7.771,17.367-17.361,17.367c-9.587,0-17.358-7.772-17.358-17.367
+	C-0.086,7.774,7.687,0,17.274,0C26.865,0,34.635,7.774,34.635,17.356z"/>
+<polyline fill="none" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+	23.242,11.347 11.182,23.406 11.148,23.377 "/>
+<polyline fill="none" stroke="#FFFFFF" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="
+	11.166,11.329 23.226,23.389 23.195,23.424 "/>
+<circle display="none" fill="#FFFFFF" cx="17.206" cy="21.064" r="1.685"/>
+<circle display="none" fill="#FFFFFF" cx="25.205" cy="21.064" r="1.686"/>
+<circle display="none" fill="#FFFFFF" cx="9.206" cy="21.064" r="1.685"/>
+</svg>

commit cec207b019884f2e174ff82bfc8d4eeb9c775ee6

IDE: fixed MSW indicator, broken since 1b4568e09b2c0c0c67c5357fb7f91ba0af348590

IDE/frontend-dev/src/Views/ToolbarView.js
@@ -120,7 +120,7 @@ class ToolbarView extends View {
         .removeClass('running')
         .removeClass('running-button');
 			$('[data-toolbar-bela-cpu]').html('CPU: --').css('color', 'black');
-  		$('[data-toolbar-msw-cpu]').html('MSW: --').css('color', 'black');
+			$('[data-toolbar-cpu-msw]').html('MSW: --').css('color', 'black');
 			modeswitches = 0;
 		}
 	}
@@ -256,10 +256,10 @@ class ToolbarView extends View {
 	}
 
 	mode_switches(value){
-		$('[data-toolbar-msw-cpu]').html('MSW: '+value);
+		$('[data-toolbar-cpu-msw]').html('MSW: '+value);
 		if (value > modeswitches){
 			this.emit('mode-switch-warning', value);
-			$('[data-toolbar-msw-cpu]').css('color', 'red');
+			$('[data-toolbar-cpu-msw]').css('color', 'red');
 		}
 		modeswitches = value;
 	}

IDE/public/js/bundle.js
@@ -4780,7 +4780,7 @@ var ToolbarView = function (_View) {
 			} else {
 				$('[data-toolbar-run]').removeClass('running').removeClass('running-button');
 				$('[data-toolbar-bela-cpu]').html('CPU: --').css('color', 'black');
-				$('[data-toolbar-msw-cpu]').html('MSW: --').css('color', 'black');
+				$('[data-toolbar-cpu-msw]').html('MSW: --').css('color', 'black');
 				modeswitches = 0;
 			}
 		}
@@ -4914,10 +4914,10 @@ var ToolbarView = function (_View) {
 	}, {
 		key: 'mode_switches',
 		value: function mode_switches(value) {
-			$('[data-toolbar-msw-cpu]').html('MSW: ' + value);
+			$('[data-toolbar-cpu-msw]').html('MSW: ' + value);
 			if (value > modeswitches) {
 				this.emit('mode-switch-warning', value);
-				$('[data-toolbar-msw-cpu]').css('color', 'red');
+				$('[data-toolbar-cpu-msw]').css('color', 'red');
 			}
 			modeswitches = value;
 		}