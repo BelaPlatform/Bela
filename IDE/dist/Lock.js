b8499a3bb6c55aebd5178191782e11003b94732e
Giulio Moro <giuliomoro@yahoo.it>
2020-11-07 17:30:30 +0000

    IDE: logging a shortened stacktrace to hope to see who's waiting

--- IDE/dist/Lock.js
+++ IDE/dist/Lock.js
@@ -1,6 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var await_lock_1 = require("await-lock");
+// Simplified stacktrace function
+function stacktrace() {
+    return new Error().stack
+        .replace("Error", "")
+        .replace(/^.*stacktrace.*\n/gm, "")
+        .replace(/^.*Lock.*\n/gm, "")
+        .replace(/\/Users\/giulio\/Dropbox\/matlab\/phd\/workspace\/Bela\/IDE\/dist\//g, "")
+        .replace(/\/root\/Bela\/IDE\/dist\//g, "")
+        .replace(/^.*new Promise \(<anonymous>\).*$/gm, "")
+        .replace(/^.*__awaiter.*$/gm, "")
+        .replace(/^\s*at <anonymous>$/gm, "")
+        .replace(/^\s*at step \(.*$/gm, "")
+        .replace(/^\s*at Object.next \(.*$/gm, "")
+        .replace(/^\s*at Object.<anonymous>.*\(.*$/gm, "")
+        .replace(/^\s*at fulfilled \(.*$/gm, "")
+        .replace(/^\s*at [/A-Za-z]+\.js:7:.*$/gm, "")
+        .replace(/Object./g, "")
+        .replace(/\n/g, "__")
+        .replace(/_[_]+/g, "__")
+        .replace(/\s+/gm, " ");
+}
 var Lock = /** @class */ (function () {
     function Lock(arg) {
         this.lock = new await_lock_1.default();
@@ -9,18 +30,27 @@ var Lock = /** @class */ (function () {
     Lock.prototype.acquire = function () {
         var ret = this.lock.tryAcquire();
         if (ret) {
-            console.log(this.arg, "FAST Acquire: ", this.lock._waitingResolvers.length);
+            if (this.arg === "FileManager") {
+                console.log(this.arg, "FAST Acquire: ", this.lock._waitingResolvers.length, stacktrace());
+            }
             return new Promise(function (resolve) { return resolve(); });
         }
         else {
+            if (this.arg === "FileManager") {
+                console.log(this.arg, "SLOW Acquiring: ", this.lock._waitingResolvers.length, stacktrace());
+            }
             var p = this.lock.acquireAsync();
-            console.log(this.arg, "SLOW Acquire: ", this.lock._waitingResolvers.length);
+            if (this.arg === "FileManager") {
+                console.log(this.arg, "SLOW Acquired: ", this.lock._waitingResolvers.length, stacktrace());
+            }
             return p;
         }
     };
     Lock.prototype.release = function () {
         this.lock.release();
-        console.log(this.arg, "Release: ", this.lock._waitingResolvers.length);
+        if (this.arg === "FileManager") {
+            console.log(this.arg, "Release: ", this.lock._waitingResolvers.length, stacktrace());
+        }
     };
     return Lock;
 }());
--- IDE/src/Lock.ts
+++ IDE/src/Lock.ts
@@ -1,5 +1,26 @@
 import AwaitLock from 'await-lock';
 
+// Simplified stacktrace function
+function stacktrace() : string{
+	return new Error().stack
+.replace("Error", "")
+.replace(/^.*stacktrace.*\n/gm, "")
+.replace(/^.*Lock.*\n/gm, "")
+.replace(/\/Users\/giulio\/Dropbox\/matlab\/phd\/workspace\/Bela\/IDE\/dist\//g, "")
+.replace(/\/root\/Bela\/IDE\/dist\//g, "")
+.replace(/^.*new Promise \(<anonymous>\).*$/gm, "")
+.replace(/^.*__awaiter.*$/gm, "")
+.replace(/^\s*at <anonymous>$/gm, "")
+.replace(/^\s*at step \(.*$/gm, "")
+.replace(/^\s*at Object.next \(.*$/gm, "")
+.replace(/^\s*at Object.<anonymous>.*\(.*$/gm, "")
+.replace(/^\s*at fulfilled \(.*$/gm, "")
+.replace(/^\s*at [/A-Za-z]+\.js:7:.*$/gm, "")
+.replace(/Object./g, "")
+.replace(/\n/g, "__")
+.replace(/_[_]+/g, "__")
+.replace(/\s+/gm, " ")
+}
 export class Lock {
 	private lock: AwaitLock;
 	private arg: string;
@@ -10,16 +31,25 @@ export class Lock {
 	acquire(): Promise<void> {
 		let ret = this.lock.tryAcquire();
 		if(ret) {
-			console.log(this.arg, "FAST Acquire: ", this.lock._waitingResolvers.length);
+			if(this.arg === "FileManager") {
+				console.log(this.arg, "FAST Acquire: ", this.lock._waitingResolvers.length, stacktrace());
+			}
 			return new Promise( (resolve) => resolve());
 		} else {
+			if(this.arg === "FileManager") {
+				console.log(this.arg, "SLOW Acquiring: ", this.lock._waitingResolvers.length, stacktrace());
+			}
 			var p = this.lock.acquireAsync();
-			console.log(this.arg, "SLOW Acquire: ", this.lock._waitingResolvers.length);
+			if(this.arg === "FileManager") {
+				console.log(this.arg, "SLOW Acquired: ", this.lock._waitingResolvers.length, stacktrace());
+			}
 			return p;
 		}
 	}
 	release(): void {
 		this.lock.release();
-		console.log(this.arg, "Release: ", this.lock._waitingResolvers.length);
+		if(this.arg === "FileManager") {
+			console.log(this.arg, "Release: ", this.lock._waitingResolvers.length, stacktrace());
+		}
 	}
 }
