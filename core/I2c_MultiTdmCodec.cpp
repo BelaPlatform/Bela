0cdfd2f320fc579ddf864489dccb03dd8b3f8013
Giulio Moro <giuliomoro@yahoo.it>
2021-04-06 15:10:10 +0000

    I2c_MultiTdmCodec: dealing better with clock source

--- core/I2c_MultiTdmCodec.cpp
+++ core/I2c_MultiTdmCodec.cpp
@@ -1,7 +1,8 @@
-#include "../include/I2c_MultiTdmCodec.h"
+#include "I2c_MultiTdmCodec.h"
 
 static const unsigned int minTdmIns = 6;
 static const unsigned int minTdmOuts = 6;
+static const AudioCodecParams::ClockSource kWclkSource = AudioCodecParams::kClockSourceExternal;
 
 I2c_MultiTLVCodec::TdmConfig I2c_MultiTdmCodec::makeTdmConfig()
 {
@@ -15,16 +16,19 @@ I2c_MultiTLVCodec::TdmConfig I2c_MultiTdmCodec::makeTdmConfig()
 I2c_MultiTdmCodec::I2c_MultiTdmCodec(const std::string& cfgString, bool isVerbose)
 	: I2c_MultiTLVCodec(cfgString, makeTdmConfig(), isVerbose)
 {
+	auto codecParams = primaryCodec->getParameters();
+	codecParams.wclk = kWclkSource;
+	primaryCodec->setParameters(codecParams);
 	I2c_MultiTLVCodec::getMcaspConfig();
 	mcaspConfig.params.inSerializers = {0, 1};
 	mcaspConfig.params.outSerializers = {2, 3};
+	mcaspConfig.params.inChannels = getNumIns();
+	mcaspConfig.params.outChannels = getNumOuts();
+	mcaspConfig.params.wclkIsInternal = AudioCodecParams::kClockSourceMcasp == codecParams.wclk;
 }
 
 McaspConfig& I2c_MultiTdmCodec::getMcaspConfig()
 {
-	mcaspConfig.params.inChannels = getNumIns();
-	mcaspConfig.params.outChannels	= getNumOuts();
-	mcaspConfig.params.wclkIsInternal = false;
 	return mcaspConfig;
 }
 
