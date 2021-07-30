54ee1568c68f4ca990db11a21bf436da4acfbae5
Dhruva_Gole <goledhruva@gmail.com>
2021-07-30 10:44:53 +0100

    PruManager: added support for RPROC (though ENABLE_PRU_UIO is still default)
    Co-authored-by: Giulio Moro <giuliomoro@yahoo.it>

--- core/PruManager.cpp
+++ core/PruManager.cpp
@@ -26,6 +26,69 @@ PruManager::PruManager(unsigned int pruNum, int v)
 PruManager::~PruManager()
 {}
 
+#if ENABLE_PRU_RPROC == 1
+const std::vector<uint32_t> prussOwnRamOffsets = {0x0, 0x2000};
+const uint32_t prussSharedRamOffset = 0x10000;
+
+PruManagerRprocMmap::PruManagerRprocMmap(unsigned int pruNum, int v) :
+	PruManager(pruNum, v)
+{
+	basePath = "/dev/remoteproc/pruss" + std::to_string(pruss) + "-core" + std::to_string(pruCore) + "/";
+	statePath = basePath + "state";
+	firmwarePath = basePath + "firmware";
+	firmware = "am57xx-pru" + std::to_string(pruss) + "_" + std::to_string(pruCore) + "-fw";
+
+#warning Untested PRU addresses for am3358
+	prussAddresses.push_back(0x4a334000);
+	prussAddresses.push_back(0x4a338000);
+}
+
+void PruManagerRprocMmap::stop()
+{
+	// performs echo stop > state
+	if(verbose)
+		printf("Stopping %s\n", pruStringId.c_str());
+	IoUtils::writeTextFile(statePath, "stop");
+}
+
+int PruManagerRprocMmap::start(bool useMcaspIrq)
+{
+#if (defined(firmwareBelaRProcNoMcaspIrq) && defined(firmwareBelaRProcMcaspIrq))
+	std::string firmwareBela = useMcaspIrq ? firmwareBelaRProcMcaspIrq : firmwareBelaRProcNoMcaspIrq; // Incoming strings from Makefile
+	return start(firmwareBela);
+#else
+#error No PRU firmware defined. Pass the name of firmware.out file using firmwareBelaRProcNoMcaspIrq=<path> and firmwareBelaRProcMcaspIrq=<path>
+#endif
+}
+
+int PruManagerRprocMmap::start(const std::string& path)
+{
+	stop();
+	std::string symlinkTarget = "/lib/firmware/" + firmware;
+	std::string firmwareCopyCommand = "ln -s -f " + path + " " + symlinkTarget;
+	system(firmwareCopyCommand.c_str());
+	if(verbose)
+		printf("Loading firmware into %s: %s symlinked from %s\n", pruStringId.c_str(), symlinkTarget.c_str(), path.c_str());
+	IoUtils::writeTextFile(firmwarePath, firmware);	// reload the new fw in PRU
+	// performs echo start > state
+	if(verbose)
+		printf("Starting %s\n", pruStringId.c_str());
+	IoUtils::writeTextFile(statePath, "start");
+	return 0;	// TODO: If system returns any error then detect it and then return 1 instead
+}
+
+void* PruManagerRprocMmap::getOwnMemory()
+{
+	return ownMemory.map(prussAddresses[pruss - 1] + prussOwnRamOffsets[pruCore], 0x2000);
+}
+
+void* PruManagerRprocMmap::getSharedMemory()
+{
+	return sharedMemory.map(prussAddresses[pruss - 1] + prussSharedRamOffset, 0x8000);
+}
+#endif // ENABLE_PRU_RPROC
+
+#if ENABLE_PRU_UIO == 1
 #include "../include/PruBinary.h"
 
 PruManagerUio::PruManagerUio(unsigned int pruNum, int v) :
@@ -93,3 +156,4 @@ void* PruManagerUio::getSharedMemory()
 	else
 		return pruSharedRam;
 }
+#endif // ENABLE_PRU_UIO
