31dd0262e6b25073efb9a27ebbd8cc628373a502
Robert Jack <robert.h.jack@gmail.com>
2021-10-25 20:43:39 +0200

    Added Multichannel Examples folder.

---
+++ examples/Multichannel/multi-sinetone/render.cpp
@@ -0,0 +1,65 @@
+/*
+ ____  _____ _        _
+| __ )| ____| |      / \
+|  _ \|  _| | |     / _ \
+| |_) | |___| |___ / ___ \
+|____/|_____|_____/_/   \_\
+http://bela.io
+*/
+/**
+\example Multichannel/multi-sinetone/render.cpp
+
+Multichannel Sintone
+--------------------
+
+This project is designed to work with the Bela Mini Multichannel Expander or the CTAG multichannel board.
+
+This project generates a sinetone with a different frequency on each of the 8 audio inputs.
+*/
+
+#include <Bela.h>
+#include <cmath>
+
+#define NUM_OUTPUTS 8
+
+float gPhases[NUM_OUTPUTS];
+float gFrequencies[NUM_OUTPUTS] = {
+	220,
+	330,
+	440,
+	550,
+	660,
+	770,
+	880,
+	990
+};
+
+float gInverseSampleRate;
+
+bool setup(BelaContext *context, void *userData)
+{
+	gInverseSampleRate = 1.0 / context->audioSampleRate;
+	for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+		gPhases[channel] = 0.0;
+	}
+	return true;
+}
+
+void render(BelaContext *context, void *userData)
+{
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+
+		for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+			float out = 0.8 * sinf(gPhases[channel]);
+			gPhases[channel] += 2.0 * M_PI * gFrequencies[channel] * gInverseSampleRate;
+			if(gPhases[channel] > 2.0 * M_PI)
+				gPhases[channel] -= 2.0 * M_PI;
+			audioWrite(context, n, channel, out);
+		}
+	}
+}
+
+void cleanup(BelaContext *context, void *userData)
+{
+
+}
