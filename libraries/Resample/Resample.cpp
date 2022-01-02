133485114b820971cc9348cdbbc11d432659f29c
fhchl <fhchl@users.noreply.github.com>
2022-01-02 23:21:03 +0100

    whitespace + sizeof()

--- libraries/Resample/Resample.cpp
+++ libraries/Resample/Resample.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <math.h>    // ceil
-#include <string.h>  // memcpy
+#include <math.h>   // ceil
+#include <string.h> // memcpy
 
 #define ENABLE_NE10_FIR_DECIMATE_FLOAT_NEON // Defines needed for Ne10 library
 #define ENABLE_NE10_FIR_INTERPOLATE_FLOAT_NEON
@@ -10,133 +10,133 @@
 #include "aa_fir.h"
 
 std::vector<float> ResampleBase::get_fir(unsigned int factor, fir_quality quality, fir_phase phase) {
-    if (factor == 2 && quality == high && phase == minimum)
-        return fir_2_high_minphase;
-    else if (factor == 2 && quality == high && phase == linear)
-        return fir_2_high_linear;
-    else if (factor == 2 && quality == low && phase == minimum)
-        return fir_2_low_minphase;
-    else if (factor == 2 && quality == low && phase == linear)
-        return fir_2_low_linear;
-    else if (factor == 4 && quality == high && phase == minimum)
-        return fir_4_high_minphase;
-    else if (factor == 4 && quality == high && phase == linear)
-        return fir_4_high_linear;
-    else if (factor == 4 && quality == low && phase == minimum)
-        return fir_4_low_minphase;
-    else if (factor == 4 && quality == low && phase == linear)
-        return fir_4_low_linear;
-    else if (factor == 8 && quality == high && phase == minimum)
-        return fir_8_high_minphase;
-    else if (factor == 8 && quality == high && phase == linear)
-        return fir_8_high_linear;
-    else if (factor == 8 && quality == low && phase == minimum)
-        return fir_8_low_minphase;
-    else if (factor == 8 && quality == low && phase == linear)
-        return fir_8_low_linear;
-    else if (factor == 16 && quality == high && phase == minimum)
-        return fir_16_high_minphase;
-    else if (factor == 16 && quality == high && phase == linear)
-        return fir_16_high_linear;
-    else if (factor == 16 && quality == low && phase == minimum)
-        return fir_16_low_minphase;
-    else if (factor == 16 && quality == low && phase == linear)
-        return fir_16_low_linear;
-    else {
-        std::cerr << "get_fir: invalid combination " << factor << quality << phase << std::endl;
-        return std::vector<float>();
-    }
+	if (factor == 2 && quality == high && phase == minimum)
+		return fir_2_high_minphase;
+	else if (factor == 2 && quality == high && phase == linear)
+		return fir_2_high_linear;
+	else if (factor == 2 && quality == low && phase == minimum)
+		return fir_2_low_minphase;
+	else if (factor == 2 && quality == low && phase == linear)
+		return fir_2_low_linear;
+	else if (factor == 4 && quality == high && phase == minimum)
+		return fir_4_high_minphase;
+	else if (factor == 4 && quality == high && phase == linear)
+		return fir_4_high_linear;
+	else if (factor == 4 && quality == low && phase == minimum)
+		return fir_4_low_minphase;
+	else if (factor == 4 && quality == low && phase == linear)
+		return fir_4_low_linear;
+	else if (factor == 8 && quality == high && phase == minimum)
+		return fir_8_high_minphase;
+	else if (factor == 8 && quality == high && phase == linear)
+		return fir_8_high_linear;
+	else if (factor == 8 && quality == low && phase == minimum)
+		return fir_8_low_minphase;
+	else if (factor == 8 && quality == low && phase == linear)
+		return fir_8_low_linear;
+	else if (factor == 16 && quality == high && phase == minimum)
+		return fir_16_high_minphase;
+	else if (factor == 16 && quality == high && phase == linear)
+		return fir_16_high_linear;
+	else if (factor == 16 && quality == low && phase == minimum)
+		return fir_16_low_minphase;
+	else if (factor == 16 && quality == low && phase == linear)
+		return fir_16_low_linear;
+	else {
+		std::cerr << "get_fir: invalid combination " << factor << quality << phase << std::endl;
+		return std::vector<float>();
+	}
 }
 
 void ResampleBase::cleanup() {
-    NE10_FREE(pCoeff);
-    NE10_FREE(pState);
-    pCoeff = nullptr;
-    pState = nullptr;
+	NE10_FREE(pCoeff);
+	NE10_FREE(pState);
+	pCoeff = nullptr;
+	pState = nullptr;
 }
 
 int Decimator::setup(unsigned int decimationFactor, unsigned int blockSize, fir_quality quality, fir_phase phase) {
-    cleanup();
-    this->blockSizeIn = blockSize;
-    this->factor      = decimationFactor;
-    if (decimationFactor == 1) {
-        // Don't do anything
-        return 0;
-    }
-    std::vector<float> fir     = get_fir(decimationFactor, quality, phase);
-    uint               numTaps = fir.size();
+	cleanup();
+	this->blockSizeIn = blockSize;
+	this->factor = decimationFactor;
+	if (decimationFactor == 1) {
+		// Don't do anything
+		return 0;
+	}
+	std::vector<float> fir = get_fir(decimationFactor, quality, phase);
+	uint numTaps = fir.size();
 
-    pCoeff = (ne10_float32_t*)NE10_MALLOC(numTaps * sizeof(ne10_float32_t));
-    pState = (ne10_float32_t*)NE10_MALLOC(
-        (numTaps + blockSize - 1) * sizeof(ne10_float32_t));
-    if (!pState || !pCoeff)
-        return -1;
+	pCoeff = (ne10_float32_t*)NE10_MALLOC(numTaps * sizeof(ne10_float32_t));
+	pState = (ne10_float32_t*)NE10_MALLOC(
+		(numTaps + blockSize - 1) * sizeof(ne10_float32_t));
+	if (!pState || !pCoeff)
+		return -1;
 
-    for (unsigned int n = 0; n < numTaps; ++n) {
-        // reverse filter coefficients
-        pCoeff[n] = fir[numTaps - 1 - n];
-    }
-    int err = ne10_fir_decimate_init_float(&decimator, numTaps, decimationFactor, pCoeff, pState, blockSize);
-    if (err) {
-        std::cerr << "ERR: blockSize must be whole multiple of decimationFactor" << std::endl;
-        return -1;
-    }
+	for (unsigned int n = 0; n < numTaps; ++n) {
+		// reverse filter coefficients
+		pCoeff[n] = fir[numTaps - 1 - n];
+	}
+	int err = ne10_fir_decimate_init_float(&decimator, numTaps, decimationFactor, pCoeff, pState, blockSize);
+	if (err) {
+		std::cerr << "ERR: blockSize must be whole multiple of decimationFactor" << std::endl;
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
 void Decimator::process(ne10_float32_t* outBlock, const ne10_float32_t* inBlock) {
-    if (factor == 1 && outBlock != inBlock) {
-        memcpy(outBlock, inBlock, blockSizeIn * sizeof *outBlock);
-        return;
-    }
-    ne10_fir_decimate_float_neon(&decimator, (float*)inBlock, outBlock, blockSizeIn);
+	if (factor == 1 && outBlock != inBlock) {
+		memcpy(outBlock, inBlock, blockSizeIn * sizeof(*outBlock));
+		return;
+	}
+	ne10_fir_decimate_float_neon(&decimator, (float*)inBlock, outBlock, blockSizeIn);
 }
 
 int Interpolator::setup(unsigned int L, unsigned int blockSize, fir_quality quality, fir_phase phase) {
-    cleanup();
-    this->blockSizeIn = blockSize / L;
-    this->factor      = L;
-    if (L == 1) {
-        // Don't do anything
-        return 0;
-    }
-    std::vector<float> fir      = get_fir(L, quality, phase);
-    uint               filtSize = fir.size();
-    // numTaps must be a multiple of the interpolation factor
-    uint numTaps     = ceil((float)filtSize / L) * L;
-    uint phaseLength = numTaps / L;
-    pCoeff           = (ne10_float32_t*)NE10_MALLOC(numTaps * sizeof(ne10_float32_t));
-    pState           = (ne10_float32_t*)NE10_MALLOC(
-        (blockSizeIn + phaseLength - 1) * sizeof(ne10_float32_t));
-    if (!pState || !pCoeff) {
-        return -1;
-    }
+	cleanup();
+	this->blockSizeIn = blockSize / L;
+	this->factor = L;
+	if (L == 1) {
+		// Don't do anything
+		return 0;
+	}
+	std::vector<float> fir = get_fir(L, quality, phase);
+	uint filtSize = fir.size();
+	// numTaps must be a multiple of the interpolation factor
+	uint numTaps = ceil((float)filtSize / L) * L;
+	uint phaseLength = numTaps / L;
+	pCoeff = (ne10_float32_t*)NE10_MALLOC(numTaps * sizeof(ne10_float32_t));
+	pState = (ne10_float32_t*)NE10_MALLOC(
+		(blockSizeIn + phaseLength - 1) * sizeof(ne10_float32_t));
+	if (!pState || !pCoeff) {
+		return -1;
+	}
 
-    for (unsigned int n = 0; n < filtSize; ++n) {
-        // reverse fir coefficients
-        pCoeff[numTaps - filtSize + n] = fir[filtSize - 1 - n];
-    }
-    for (uint n = 0; n < numTaps - filtSize; ++n) {
-        // rest are are zeros
-        pCoeff[n] = 0;
-    }
+	for (unsigned int n = 0; n < filtSize; ++n) {
+		// reverse fir coefficients
+		pCoeff[numTaps - filtSize + n] = fir[filtSize - 1 - n];
+	}
+	for (uint n = 0; n < numTaps - filtSize; ++n) {
+		// rest are are zeros
+		pCoeff[n] = 0;
+	}
 
-    int err = ne10_fir_interpolate_init_float(&interpolator, L, numTaps, pCoeff, pState, blockSizeIn);
-    if (err) {
-        std::cerr << "Error: couldn't init interpolator" << std::endl;
-        return -1;
-    }
+	int err = ne10_fir_interpolate_init_float(&interpolator, L, numTaps, pCoeff, pState, blockSizeIn);
+	if (err) {
+		std::cerr << "Error: couldn't init interpolator" << std::endl;
+		return -1;
+	}
 
-    return 0;
+	return 0;
 }
 
 void Interpolator::process(ne10_float32_t* outBlock, const ne10_float32_t* inBlock) {
-    if (factor == 1 && outBlock != inBlock) {
-        memcpy(outBlock, inBlock, blockSizeIn * sizeof *outBlock);
-        return;
-    }
-    ne10_fir_interpolate_float_neon(&interpolator, (float*)inBlock, outBlock, blockSizeIn);
+	if (factor == 1 && outBlock != inBlock) {
+		memcpy(outBlock, inBlock, blockSizeIn * sizeof(*outBlock));
+		return;
+	}
+	ne10_fir_interpolate_float_neon(&interpolator, (float*)inBlock, outBlock, blockSizeIn);
 }
 
 #if 0
