44d22217e8ba4b988cc9f32e654206709a55f8fa
Giulio Moro <giuliomoro@yahoo.it>
2022-01-25 16:19:36 +0000

    benchmark.sh: pre-building libraries

--- resources/tests/benchmark.sh
+++ resources/tests/benchmark.sh
@@ -9,6 +9,10 @@ EXAMPLES=(
 	Salt/pink-trombone
 )
 PROJECTS=() # automatically populated below
+# manually list libraries required by the examples listed.
+# If a library is skipped here it will be built during build
+# which may affect "compile" time the first time the project is built
+LIBRARIES="Biquad Gui math_neon Midi Pipe Scope Trill"
 
 SETUP=0
 BUILD=0
@@ -62,7 +66,10 @@ done
 
 if [ 1 -eq $BUILD ]; then
 	echo "========build========"
-	# ensure everything else is built before we go ahead and time the project build
+	# ensure mostly everything else is built before we go ahead and time the project build
+	for l in $LIBRARIES; do
+		make -f Makefile.libraries LIBRARY=$l > /dev/null
+	done
 	make lib > /dev/null
 	make PROJECT=${PROJECTS[0]} build/core/default_libpd_render.o build/core/default_main.o > /dev/null
 	for p in "${PROJECTS[@]}"; do
@@ -79,6 +86,7 @@ if [ 1 -eq $BUILD ]; then
 				eval "time make PROJECT=$p $objs" 2>&1 | grep real | sed 's/real[ \t]*//' | cat <(printf "$p compile: ") -
 			fi
 			eval "time make PROJECT=$p" 2>&1 | grep real | sed 's/real[ \t]*//' | cat <(printf "$p link: ") -
+			make PROJECT=$p > /dev/null # this is to make sure any build error from the line above show up here
 		fi
 	done
 fi
