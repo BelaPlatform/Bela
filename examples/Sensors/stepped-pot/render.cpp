39b9c04a2eb41ce6c84fd133d35dd8b1f383676e
Robert Jack <robert.h.jack@gmail.com>
2022-03-28 11:36:56 +0200

    stepped-pot sonification

--- examples/Sensors/stepped-pot/render.cpp
+++ examples/Sensors/stepped-pot/render.cpp
@@ -10,17 +10,29 @@ http://bela.io
 \example Sensors/stepped-pot/render.cpp
 
 Stepped potentiometer
-================
+=====================
 
-Connect a potentiometer (stepped or not) to Bela's analog input 0, keep track
-of when it changes from one step to the next.
+Connect a potentiometer (stepped or not) to Bela's analog input 0.
+This project will keep track of when it changes from one step to the next.
+
+We define the steps in volts and list them in steppedPot.setup().
+
+In this example we use the quantised range of the pot to change the frequecy of
+a sinetone oscillator. We read the position integer to multiply the base frequency
+and cycle through the harmonic series.
 */
 
 #include <Bela.h>
+#include <cmath>
 #include <libraries/SteppedPot/BelaSteppedPot.h>
 
 const unsigned int kPotInChannel = 0;
 BelaSteppedPot steppedPot;
+int gStepNumber = 0;
+
+float gFrequency = 220.0;
+float gPhase;
+float gInverseSampleRate;
 
 bool setup(BelaContext *context, void *userData)
 {
@@ -43,6 +55,8 @@ bool setup(BelaContext *context, void *userData)
 		0.1, // tolerance (in Volt) around the nominal value stated above
 		4.096 // this is the Bela ADC's full-scale input
 	);
+	gInverseSampleRate = 1.0 / context->audioSampleRate;
+	gPhase = 0.0;
 	return true;
 }
 
@@ -52,6 +66,18 @@ void render(BelaContext *context, void *userData)
 	if(hasChanged)
 	{
 		rt_printf("Pot has moved to position %u\n", steppedPot.get());
+		gStepNumber = steppedPot.get();
+	}
+
+	for(unsigned int n = 0; n < context->audioFrames; n++) {
+		float out = 0.8f * sinf(gPhase);
+		gPhase += 2.0f * (float)M_PI * ((gStepNumber + 1) * gFrequency) * gInverseSampleRate;
+		if(gPhase > M_PI)
+			gPhase -= 2.0f * (float)M_PI;
+
+		for(unsigned int channel = 0; channel < context->audioOutChannels; channel++) {
+			audioWrite(context, n, channel, out);
+		}
 	}
 }
 
