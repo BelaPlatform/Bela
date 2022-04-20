887f4090ce386e7eb83f55d5d0e215e4fee384fb
Giulio Moro <giuliomoro@yahoo.it>
2022-04-20 18:26:59 +0200

    heavy: obsoleted old build_pd_heavy.sh, added build_pd_hvcc.sh which allows to build with a local build of hvcc

---
+++ scripts/build_pd_hvcc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -eo pipefail
+SOURCE=$1
+shift
+[ -z "$SOURCE" ] && {
+	echo "Usage: \`$0 path/to/project [ options to build_project.sh ]'" >&2
+	exit 1;
+}
+[ -d $SOURCE ] && {
+	SOURCE=$SOURCE/_main.pd
+}
+[ -z $PROJECT_NAME ] && PROJECT_NAME=$(basename $(dirname $SOURCE))
+
+SCRIPTDIR=$(dirname "$0")
+[ -z "$SCRIPTDIR" ] && SCRIPTDIR="./" || SCRIPTDIR=$SCRIPTDIR/
+[ -z "$tmppath" ] && tmppath="$SCRIPTDIR/../tmp/heavy/hvtemp/"
+. $SCRIPTDIR.bela_common || { echo "You must be in Bela/scripts to run these scripts" | exit 1; }
+set -u
+
+hvcc $SOURCE -o $tmppath -n bela -g c
+rsync -aq $SCRIPTDIR/hvresources/render.cpp $tmppath/c
+$SCRIPTDIR/build_project.sh $tmppath/c/ -p $PROJECT_NAME $@
