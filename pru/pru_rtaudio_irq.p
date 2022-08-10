BelaPlatform/Bela

commit 1a67a8bf7e9d7e104aebf4f8e94f24ed8b2a9798

pru_rtaudio_irq.p: use new DO_DIGITAL macro. This ensures that digital outs are cleared in case ARM crashes

pru/pru_rtaudio_irq.p
@@ -2207,12 +2207,7 @@ PROCESS_SPI_END:
 
      // Skip digital processing if digital IOs are disabled
      QBBC PROCESS_DIGITAL_END, reg_flags, FLAG_BIT_USE_DIGITAL
-
-     //r27 is actually r27, so do not use r27 from here to ...
-     LBBO r27, reg_digital_current, 0, 4 
-     JAL r28.w0, DIGITAL // note that this is not called as a macro, but with JAL. r28 will contain the return address
-     SBBO r27, reg_digital_current, 0,   4 
-     //..here you can start using r27 again
+     DO_DIGITAL
 
      ADD reg_digital_current, reg_digital_current, 4 //increment pointer
 

commit 42bbf18c3710ed82cdf24b09ab72ac2239bf148e

pru_rtaudio_irq.p: fixed digital outs. This was broken since 1a67a8bf7e9d7e104aebf4f8e94f24ed8b2a9798

pru/pru_rtaudio_irq.p
@@ -2276,8 +2276,6 @@ PROCESS_SPI_END:
      QBBC PROCESS_DIGITAL_END, reg_flags, FLAG_BIT_USE_DIGITAL
      DO_DIGITAL
 
-     ADD reg_digital_current, reg_digital_current, 4 //increment pointer
-
 PROCESS_DIGITAL_END:
 
 	 XOR reg_flags, reg_flags, (1 << FLAG_BIT_MCASP_RX_FIRST_FRAME) // Toggle frame flag