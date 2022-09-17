dc86c684d38a1a7994938485406927d086230e74
Giulio Moro <giuliomoro@yahoo.it>
2022-09-17 14:06:52 +0000

    PureData/gui-send: using two buffers

--- examples/PureData/gui-send/sketch.js
+++ examples/PureData/gui-send/sketch.js
@@ -4,22 +4,29 @@ function setup() {
 
 function draw () {
         
+    if(Bela.data.buffers.length < 2) {
+        // haven't received enough data yet
+        return;
+    }
     // Draw a white background with opacity
     background(0, 50);
     
-    // Retreive the data being sent from render.cpp
-    let sine = Bela.data.buffers[0];
-        
-    let circlePosition = windowWidth/5;
+    // Retrieve the data being sent from the Pd patch
+    let pdBuf0 = Bela.data.buffers[0];
+    let pdBuf1 = Bela.data.buffers[1];
+    let numCircles = min(pdBuf0.length, pdBuf1.length);
+
+    let circlePosition = windowWidth/(numCircles + 1);
         
     // Draw a circle whose size changes with the value received from render.cpp
     noFill();
     stroke(255);
     strokeWeight(10);
         
-    for (var i = 0; i < 4; i++) {
-      const thickness = 30 * abs(sine[i]);
-      strokeWeight(thickness);
-      ellipse(circlePosition + (circlePosition*i), windowHeight / 2, 200, 200);
+    for (var i = 0; i < numCircles; i++) {
+        const thickness = 30 * abs(pdBuf0[i]);
+        stroke(255 * abs(pdBuf1[i]));
+        strokeWeight(thickness);
+        ellipse(circlePosition + (circlePosition*i), windowHeight / 2, 200, 200);
     }   
 }
