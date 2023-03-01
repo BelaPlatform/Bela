305c64b8c23c0150f3f45b8538c65c1f4b5ae76c
Giulio Moro <giuliomoro@yahoo.it>
2023-03-01 01:02:56 +0000

    Serial: handling all known baudrates

--- libraries/Serial/Serial.cpp
+++ libraries/Serial/Serial.cpp
@@ -33,47 +33,71 @@ int Serial::setup (const char* device, unsigned int speed) {
 }
 
 unsigned int Serial::speedToBaudRate(unsigned int speed) {
+	// use the below to generate the body of the switch statement (need to add default:)
+// grep "define[ ]\+B[0-9]\+" /usr/include/arm-linux-gnueabihf/bits/termios.h | sed "s/\s\+/ /g" | cut -d ' ' -f 2 | sed "s/\(B\)\([0-9]\+\)/\t\tcase \2: speed = \1\2;\n\t\tbreak;/"
 	switch(speed) {
-		case 50:    	speed = B50;
-		break;
-		case 75:    	speed = B75;
-		break;
-		case 110:   	speed = B110;
-		break;
-		case 134:   	speed = B134;
-		break;
-		case 150:   	speed = B150;
-		break;
-		case 200:   	speed = B200;
-		break;
-		case 300:   	speed = B300;
-		break;
-		case 600:   	speed = B600;
-		break;
-		case 1200:  	speed = B1200;
-		break;
-		case 1800:  	speed = B1800;
-		break;
-		case 2400:  	speed = B2400;
-		break;
-		case 4800:  	speed = B4800;
-		break;
-		case 9600:  	speed = B9600;
-		break;
-		case 19200: 	speed = B19200;
-		break;
-		case 38400: 	speed = B38400;
-		break;
-		case 57600: 	speed = B57600;
-		break;
-		case 115200:	speed = B115200;
-		break;
-		case 230400:	speed = B230400;
-		break;
-		case 0:
-		//nobreak
 		default:
-		speed = B0;
+		case 0: speed = B0;
+		break;
+		case 50: speed = B50;
+		break;
+		case 75: speed = B75;
+		break;
+		case 110: speed = B110;
+		break;
+		case 134: speed = B134;
+		break;
+		case 150: speed = B150;
+		break;
+		case 200: speed = B200;
+		break;
+		case 300: speed = B300;
+		break;
+		case 600: speed = B600;
+		break;
+		case 1200: speed = B1200;
+		break;
+		case 1800: speed = B1800;
+		break;
+		case 2400: speed = B2400;
+		break;
+		case 4800: speed = B4800;
+		break;
+		case 9600: speed = B9600;
+		break;
+		case 19200: speed = B19200;
+		break;
+		case 38400: speed = B38400;
+		break;
+		case 57600: speed = B57600;
+		break;
+		case 115200: speed = B115200;
+		break;
+		case 230400: speed = B230400;
+		break;
+		case 460800: speed = B460800;
+		break;
+		case 500000: speed = B500000;
+		break;
+		case 576000: speed = B576000;
+		break;
+		case 921600: speed = B921600;
+		break;
+		case 1000000: speed = B1000000;
+		break;
+		case 1152000: speed = B1152000;
+		break;
+		case 1500000: speed = B1500000;
+		break;
+		case 2000000: speed = B2000000;
+		break;
+		case 2500000: speed = B2500000;
+		break;
+		case 3000000: speed = B3000000;
+		break;
+		case 3500000: speed = B3500000;
+		break;
+		case 4000000: speed = B4000000;
 		break;
 	}
 	return speed;
