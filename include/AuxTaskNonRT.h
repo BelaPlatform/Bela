b9bea9fe81d1122cf4945e3924623e2ba4e022b7
Giulio Moro <giuliomoro@yahoo.it>
2024-05-29 18:11:10 -0500

    RtWrappers: createBelaRtPipe now returns two file descriptors: one for the rt side and one for the non-rt side. Simplified calling code accordingly

--- include/AuxTaskNonRT.h
+++ include/AuxTaskNonRT.h
@@ -15,7 +15,6 @@ private:
 	int commsInit();
 	int commsSend(const void* buf, size_t size);
 	ssize_t commsReceive(char* buf, size_t size);
-	int openPipe();
 	int pipeSocket = -1;
 	int pipe_fd = -1;
 };
