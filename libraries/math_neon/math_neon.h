62f727fe3f8b6e9a615eccad9784f4c7a5d8175e
Giulio Moro <giuliomoro@yahoo.it>
2024-05-30 19:25:15 -0500

    math_neon: workaround so we can build and link without it.

--- libraries/math_neon/math_neon.h
+++ libraries/math_neon/math_neon.h
@@ -1,2 +1,12 @@
 #pragma once
+#if __has_include_next(<math_neon.h>)
 #include_next <math_neon.h>
+#else
+#include <math.h>
+// TODO: add more
+#define sqrtf_neon(a) sqrtf(a)
+#define sinf_neon(a) sinf(a)
+#define cosf_neon(a) cosf(a)
+#define expf_neon(a) expf(a)
+#define logf_neon(a) logf(a)
+#endif
