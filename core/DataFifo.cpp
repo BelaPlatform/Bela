3feffd61a14ca3bf6d63bd37ac4f522dc70b06b0
Giulio Moro <giuliomoro@yahoo.it>
2024-07-02 02:08:12 +0000

    DataFifo: modernised, extended API, more useful error messages, more unique name

--- core/DataFifo.cpp
+++ core/DataFifo.cpp
@@ -1,6 +1,9 @@
 #include <DataFifo.h>
 #include <sys/errno.h>
 #include <time.h>
+#include "RtWrappers.h"
+#include <string.h>
+#include <errno.h>
 
 DataFifo::DataFifo()
 {
@@ -11,44 +14,62 @@ DataFifo::~DataFifo()
 	cleanup();
 }
 
-int DataFifo::setup(const std::string& name, size_t msgSize, size_t maxMsg, bool blocking, bool recreate)
+int DataFifo::setup(const std::string& name, size_t msgSize, size_t maxMsg, bool blocking, bool recreate, bool fallback)
 {
-	this->msgSize = msgSize;
+	Bela_initRtBackend();
+	qName = std::string("/q_") + name + std::to_string((long long unsigned)(this));
 	struct mq_attr attr;
 	attr.mq_maxmsg = maxMsg;
 	attr.mq_msgsize = msgSize;
 	// Check if queue already exists
-	queue = BELA_RT_WRAP(mq_open(name.c_str(), O_RDWR));
+	queue = BELA_RT_WRAP(mq_open(qName.c_str(), O_RDWR));
 	if((mqd_t)-1 != queue)
 	{
 		if(recreate)
 		{
 			// if there already is a queue with this name, try to close it.
 			BELA_RT_WRAP(mq_close(queue));
-			BELA_RT_WRAP(mq_unlink(name.c_str()));
+			BELA_RT_WRAP(mq_unlink(qName.c_str()));
 		}
 	}		
 	// Open a new queue
 	int flags = O_CREAT | O_RDWR | (blocking ? 0 : O_NONBLOCK);
-	queue = BELA_RT_WRAP(mq_open(name.c_str(), flags, 0644, &attr));
+	queue = BELA_RT_WRAP(mq_open(qName.c_str(), flags, 0644, &attr));
+	bool systemDefaults = true;
 	if((mqd_t)-1 == queue)
-		return -errno;
+	{
+		if(fallback)
+		{
+			systemDefaults = true;
+			// try with the system defaults
+			fprintf(stderr, "Opening queue %s with desired settings failed, trying with system defaults\n", qName.c_str());
+			queue = BELA_RT_WRAP(mq_open(qName.c_str(), O_CREAT | O_RDWR, 0644, nullptr));
+		}
+		if((mqd_t)-1 == queue)
+		{
+
+			fprintf(stderr, "Error creating queue %s: %d %s\n", qName.c_str(), errno, strerror(errno));
+			return -errno;
+		}
+	}
 	struct mq_attr newAttr;
 	BELA_RT_WRAP(mq_getattr(queue, &newAttr));
 	// verify that all settings have been applied (a reason for failure
 	// could be that the queue was not successfully closed above
-	if(recreate && (
+	if((recreate || systemDefaults) && (
 			newAttr.mq_maxmsg != attr.mq_maxmsg
 			|| newAttr.mq_msgsize != attr.mq_msgsize
 			|| ((newAttr.mq_flags & flags) == flags)
 		       )
 	  )
-		return -1;
-	qName = name;
+	{
+		fprintf(stderr, "Queue %s has following attributes: mq_maxmsg: %ld, mq_msgsize: %ld, mq_curmsgs: %ld, mq_flags: %ld\n", qName.c_str(), newAttr.mq_maxmsg, newAttr.mq_msgsize, newAttr.mq_curmsgs, newAttr.mq_flags);
+	}
+	queueValid = true;
 	return 0;
 }
 
-int DataFifo::send(const char* buf, size_t size)
+int DataFifo::send(const void* buf, size_t size)
 {
 	int ret = BELA_RT_WRAP(mq_send(queue, (const char*)buf, size, 0));
 	if(ret != 0)
@@ -56,24 +77,29 @@ int DataFifo::send(const char* buf, size_t size)
 	return 0;
 }
 
-int DataFifo::receive(char* buf, double timeoutMs)
+int DataFifo::receive(void* buf, size_t size, double timeoutMs)
 {
 	unsigned int prio;
 	ssize_t ret;
-	if(timeoutMs)
+	if(timeoutMs >= 0)
 	{
 		struct timespec timeout;
-		BELA_RT_WRAP(clock_gettime(CLOCK_REALTIME, &timeout));
-		//struct timespec timeoutbak = timeout;
-		long oneSecondNs = 1000000000;
-		time_t timeoutS = (time_t)(timeoutMs / 1000);
-		long timeoutNs = (timeoutMs - timeoutS * 1000) * 1000000;
-		timeout.tv_sec -= timeoutS;
-		timeout.tv_nsec += timeoutNs;
-		while(timeout.tv_nsec > oneSecondNs)
+		if(0 == timeoutMs)
 		{
-			timeout.tv_nsec -= oneSecondNs;
-			timeout.tv_sec += 1;
+			timeout = {0, 0};
+		} else {
+			BELA_RT_WRAP(clock_gettime(CLOCK_REALTIME, &timeout));
+			//struct timespec timeoutbak = timeout;
+			long oneSecondNs = 1000000000;
+			time_t timeoutS = (time_t)(timeoutMs / 1000);
+			long timeoutNs = (timeoutMs - timeoutS * 1000) * 1000000;
+			timeout.tv_sec -= timeoutS;
+			timeout.tv_nsec += timeoutNs;
+			while(timeout.tv_nsec > oneSecondNs)
+			{
+				timeout.tv_nsec -= oneSecondNs;
+				timeout.tv_sec += 1;
+			}
 		}
 #if 0
 		time_t actualS = timeoutbak.tv_sec - timeout.tv_sec;
@@ -85,9 +111,9 @@ int DataFifo::receive(char* buf, double timeoutMs)
 					timeout.tv_sec, timeout.tv_nsec
 					);
 #endif
-		ret = BELA_RT_WRAP(mq_timedreceive(queue, buf, msgSize, &prio, &timeout));
+		ret = BELA_RT_WRAP(mq_timedreceive(queue, (char*)buf, size, &prio, &timeout));
 	} else {
-		ret = BELA_RT_WRAP(mq_receive(queue, buf, msgSize, &prio));
+		ret = BELA_RT_WRAP(mq_receive(queue, (char*)buf, size, &prio));
 	}
 
 	if(ret < 0)
@@ -95,15 +121,19 @@ int DataFifo::receive(char* buf, double timeoutMs)
 	return ret;
 }
 
-int DataFifo::cleanup()
+void DataFifo::cleanup()
 {
-	int ret = BELA_RT_WRAP(mq_close(queue));
-	if(ret < 0)
-		return -errno;
-	ret = BELA_RT_WRAP(mq_unlink(qName.c_str()));
-	if(ret <0)
-		return -errno;
-	return 0;
+	if(queueValid)
+	{
+		int ret = BELA_RT_WRAP(mq_close(queue));
+		if(ret)
+			fprintf(stderr, "Error closing queue %s: %d %s\n", qName.c_str(), errno, strerror(errno));
+		ret = BELA_RT_WRAP(mq_unlink(qName.c_str()));
+		if(ret)
+			fprintf(stderr, "Error unlinking queue %s: %d %s\n", qName.c_str(), errno, strerror(errno));
+	}
+	queueValid = false;
+	queue = (mqd_t)-1;
 }
 
 #include <vector>
@@ -136,12 +166,12 @@ bool DataFifo::test()
 	}
 	int ret = df.setup("/testname", msgSize, numMsg, false);
 	assert(0 == ret);
-	ret = df.receive(received.data());
+	ret = df.receive(received.data(), msgSize);
 	assert(-EAGAIN == ret);
 
 	ret = df.send(sent.data(), msgSize);
 	assert(0 == ret);
-	ret = df.receive(received.data());
+	ret = df.receive(received.data(), msgSize);
 	assert(ret == msgSize);
 	assert(arrayEqual(sent.data(), received.data(), msgSize));
 
@@ -152,12 +182,12 @@ bool DataFifo::test()
 	ret = df.send(sent.data(), newsz);
 	assert(0 == ret);
 	fillArray(received);
-	ret = df.receive(received.data());
+	ret = df.receive(received.data(), msgSize);
 	assert(ret == newsz);
 	assert(arrayEqual(sent.data(), received.data(), newsz));
 
 	// ensure the queue is empty
-	assert(-EAGAIN == df.receive(received.data()));
+	assert(-EAGAIN == df.receive(received.data(), msgSize));
 	fillArray(received);
 	// fill up the queue
 	for(unsigned int n = 0; n < sent.size(); n += msgSize)
@@ -172,12 +202,12 @@ bool DataFifo::test()
 	// now drain the queue
 	for(unsigned int n = 0; n < received.size(); n += msgSize)
 	{
-		ret = df.receive(&received[n]);
+		ret = df.receive(&received[n], msgSize);
 		assert(msgSize == ret);
 	}
 	assert(arrayEqual(sent.data(), received.data(), sent.size()));
 	// ensure the queue is empty
-	assert(-EAGAIN == df.receive(received.data()));
+	assert(-EAGAIN == df.receive(received.data(), msgSize));
 
 	return true;
 }
